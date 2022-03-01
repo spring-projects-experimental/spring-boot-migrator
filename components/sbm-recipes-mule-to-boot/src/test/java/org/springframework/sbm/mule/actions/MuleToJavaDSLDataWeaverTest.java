47eeb9d4b2838814e89e0d81e029f7f889b21b23
Andrei Shakirin <ashakirin@.gmail.com>
2022-03-01 16:00:07 +0100

    Ping unknown data weaver test

---
+++ components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLDataWeaverTest.java
@@ -0,0 +1,106 @@
+package org.springframework.sbm.mule.actions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.SourceFile;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.sbm.engine.context.ProjectContext;
+import org.springframework.sbm.mule.actions.javadsl.translators.MuleComponentToSpringIntegrationDslTranslator;
+import org.springframework.sbm.mule.actions.javadsl.translators.common.ExpressionLanguageTranslator;
+import org.springframework.sbm.mule.actions.javadsl.translators.http.HttpListenerConfigTypeAdapter;
+import org.springframework.sbm.mule.actions.javadsl.translators.http.HttpListenerTranslator;
+import org.springframework.sbm.mule.actions.javadsl.translators.logging.LoggingTranslator;
+import org.springframework.sbm.mule.api.MuleMigrationContextFactory;
+import org.springframework.sbm.mule.api.toplevel.FlowTopLevelElementFactory;
+import org.springframework.sbm.mule.api.toplevel.SubflowTopLevelElementFactory;
+import org.springframework.sbm.mule.api.toplevel.TopLevelElementFactory;
+import org.springframework.sbm.mule.api.toplevel.configuration.ConfigurationTypeAdapterFactory;
+import org.springframework.sbm.mule.api.toplevel.configuration.MuleConfigurationsExtractor;
+import org.springframework.sbm.mule.resource.MuleXmlProjectResourceRegistrar;
+import org.springframework.sbm.project.resource.ApplicationProperties;
+import org.springframework.sbm.project.resource.RewriteSourceFileHolder;
+import org.springframework.sbm.project.resource.TestProjectContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class MuleToJavaDSLDataWeaverTest {
+    private final String xmlDW =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<mule xmlns:http=\"http://www.mulesoft.org/schema/mule/http\" \n" +
+            "  xmlns=\"http://www.mulesoft.org/schema/mule/core\"\n" +
+            "  xmlns:dw=\"http://www.mulesoft.org/schema/mule/ee/dw\"\n" +
+            "  xmlns:doc=\"http://www.mulesoft.org/schema/mule/documentation\"\n" +
+            "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd\n" +
+            "  http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd\n" +
+            "  http://www.mulesoft.org/schema/mule/ee/dw http://www.mulesoft.org/schema/mule/ee/dw/current/dw.xsd\"\n" +
+            ">\n" +
+            "  <flow name=\"http-routeFlow\" doc:id=\"ff11723e-78e9-4cc2-b760-2bec156ef0f2\" >\n" +
+            "    <http:listener doc:name=\"Listener\" doc:id=\"9f602d5c-5386-4fc9-ac8f-024d754c17e5\" config-ref=\"HTTP_Listener_Configuration\" path=\"/test\"/>\n" +
+            "    <dw:transform-message doc:name=\"mapCustomerDetailsRequestFixedwidth\">\n" +
+            "         <dw:set-payload resource=\"classpath:dwl/mapClientRiskRatingRequest.dwl\"/>\n" +
+            "    </dw:transform-message>\n" +
+            "  </flow>\n" +
+            "</mule>";
+
+    private JavaDSLAction2 myAction;
+    private final ApplicationEventPublisher eventPublisher = mock(ApplicationEventPublisher.class);
+
+    @BeforeEach
+    public void setup() {
+        List<MuleComponentToSpringIntegrationDslTranslator> translators = List.of(
+                new HttpListenerTranslator(),
+                new LoggingTranslator(new ExpressionLanguageTranslator()));
+        List<TopLevelElementFactory> topLevelTypeFactories = List.of(
+                new FlowTopLevelElementFactory(translators),
+                new SubflowTopLevelElementFactory(translators)
+        );
+
+        ConfigurationTypeAdapterFactory configurationTypeAdapterFactory = new ConfigurationTypeAdapterFactory(List.of(new HttpListenerConfigTypeAdapter()));
+        MuleMigrationContextFactory muleMigrationContextFactory = new MuleMigrationContextFactory(new MuleConfigurationsExtractor(configurationTypeAdapterFactory));
+        myAction = new JavaDSLAction2(muleMigrationContextFactory, topLevelTypeFactories);
+        myAction.setEventPublisher(eventPublisher);
+    }
+
+    @Test
+    public void shouldGenerateCommentForDWStatement() {
+        MuleXmlProjectResourceRegistrar registrar = new MuleXmlProjectResourceRegistrar();
+        ApplicationProperties applicationProperties = new ApplicationProperties();
+        applicationProperties.setDefaultBasePackage("com.example.javadsl");
+
+        ProjectContext projectContext = TestProjectContext.buildProjectContext(eventPublisher)
+                .addProjectResource("src/main/resources/mule-simple-http-flow.xml", xmlDW)
+                .withApplicationProperties(applicationProperties)
+                .withBuildFileHavingDependencies(
+                        "org.springframework:spring-context:5.3.1",
+                        "org.springframework:spring-beans:5.3.1",
+                        "org.springframework.integration:spring-integration-core:5.5.8",
+                        "org.springframework.integration:spring-integration-http:5.5.8",
+                        "org.springframework.boot:spring-boot-starter-integration:2.6.3"
+                )
+                .addRegistrar(registrar)
+                .build();
+        myAction.apply(projectContext);
+        assertThat(projectContext.getProjectJavaSources().list().size()).isEqualTo(1);
+        assertThat(projectContext.getProjectJavaSources().list().get(0).print())
+                .isEqualTo("package com.example.javadsl;\n" +
+                        "import org.springframework.context.annotation.Bean;\n" +
+                        "import org.springframework.context.annotation.Configuration;\n" +
+                        "import org.springframework.integration.dsl.IntegrationFlow;\n" +
+                        "import org.springframework.integration.dsl.IntegrationFlows;\n" +
+                        "import org.springframework.integration.handler.LoggingHandler;\n" +
+                        "import org.springframework.integration.http.dsl.Http;\n" +
+                        "\n" +
+                        "@Configuration\n" +
+                        "public class FlowConfigurations {\n" +
+                        "    @Bean\n" +
+                        "    IntegrationFlow http_routeFlow() {\n" +
+                        "        return IntegrationFlows.from(Http.inboundChannelAdapter(\"/test\")).handle((p, h) -> p)\n" +
+                        "                // FIXME: cannot convert dw:transform-message statement" +
+                        "                .get();\n" +
+                        "    }}");
+    }
+}
