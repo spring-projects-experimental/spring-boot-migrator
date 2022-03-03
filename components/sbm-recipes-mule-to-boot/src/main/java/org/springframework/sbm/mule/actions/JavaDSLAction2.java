bd01dea6ee9c5b2d5dd3cfb7cca7409b1b5e7845
Andrei Shakirin <ashakirin@.gmail.com>
2022-03-03 15:19:22 +0100

    Improved dependency coordination

--- components/sbm-recipes-mule-to-boot/src/main/java/org/springframework/sbm/mule/actions/JavaDSLAction2.java
+++ components/sbm-recipes-mule-to-boot/src/main/java/org/springframework/sbm/mule/actions/JavaDSLAction2.java
@@ -112,7 +112,6 @@ public class JavaDSLAction2 extends AbstractAction {
     private List<Dependency> buildDependencies(TopLevelElement snippet) {
         return snippet.getRequiredDependencies().stream()
                 .map(Dependency::fromCoordinates)
-                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
