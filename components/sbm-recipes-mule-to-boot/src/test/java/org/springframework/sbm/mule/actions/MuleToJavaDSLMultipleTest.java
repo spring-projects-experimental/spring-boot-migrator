961d4220f581f02dea1ca8a0f5df2a1dd5da35d5
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-03-10 11:07:00 +0000

    Refactor: MuleToJavaDSLMultipleTest is runs faster less duplicated code, removes duplicated tests

--- components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLMultipleTest.java
+++ components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLMultipleTest.java
@@ -18,49 +18,12 @@ package org.springframework.sbm.mule.actions;
 import org.junit.jupiter.api.Test;
 import org.springframework.sbm.boot.properties.api.SpringBootApplicationProperties;
 import org.springframework.sbm.boot.properties.search.SpringBootApplicationPropertiesResourceListFilter;
-import org.springframework.sbm.engine.context.ProjectContext;
-import org.springframework.sbm.mule.resource.MuleXml;
-import org.springframework.sbm.mule.resource.MuleXmlProjectResourceFilter;
-import org.springframework.sbm.mule.resource.MuleXmlProjectResourceRegistrar;
-import org.springframework.sbm.project.resource.ApplicationProperties;
-import org.springframework.sbm.project.resource.TestProjectContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MuleToJavaDSLMultipleTest extends JavaDSLActionBaseTest {
-    private final static String muleXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "\n" +
-            "<mule xmlns:amqp=\"http://www.mulesoft.org/schema/mule/amqp\" xmlns:http=\"http://www.mulesoft.org/schema/mule/http\" xmlns=\"http://www.mulesoft.org/schema/mule/core\" xmlns:doc=\"http://www.mulesoft.org/schema/mule/documentation\"\n" +
-            "xmlns:spring=\"http://www.springframework.org/schema/beans\" \n" +
-            "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-            "xsi:schemaLocation=\"http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-current.xsd\n" +
-            "http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd\n" +
-            "http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd\n" +
-            "http://www.mulesoft.org/schema/mule/amqp http://www.mulesoft.org/schema/mule/amqp/current/mule-amqp.xsd\">\n" +
-            "<amqp:connector name=\"amqpConnector\"\n" +
-            "  host=\"localhost\"\n" +
-            "  port=\"5672\"\n" +
-            "  username=\"guest\"\n" +
-            "  password=\"guest\"\n" +
-            "  doc:name=\"AMQP-0-9 Connector\"\n" +
-            "   />\n" +
-            "<flow name=\"amqp-muleFlow\">\n" +
-            "<amqp:inbound-endpoint \n" +
-            "queueName=\"sbm-integration-queue-one\"\n" +
-            "connector-ref=\"amqpConnector\"\n" +
-            "/>\n" +
-            "<!-- <http:listener config-ref=\"HTTP_Listener_Configuration\" path=\"/test\" allowedMethods=\"POST\" doc:name=\"Recieve HTTP request\"/> -->\n" +
-            "<logger message=\"payload to be sent: #[new String(payload)]\" level=\"INFO\" doc:name=\"Log the message content to be sent\"/>\n" +
-            "        <amqp:outbound-endpoint \n" +
-            "        exchangeName=\"sbm-integration-exchange\" \n" +
-            "        routingKey=\"sbm-integration-queue-two\"\n" +
-            "        responseTimeout=\"10000\"  \n" +
-            "        doc:name=\"Send to AMQP queue\"\n" +
-            "        />\n" +
-            "</flow>\n" +
-            "</mule>\n";
 
     private static final String muleInboundOutboundXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
             "\n" +
@@ -95,22 +58,21 @@ public class MuleToJavaDSLMultipleTest extends JavaDSLActionBaseTest {
             "</mule>\n";
 
     @Test
-    public void generatesAmqpDSLStatements() {
-
-        ProjectContext projectContext = TestProjectContext.buildProjectContext(eventPublisher)
-                .addProjectResource("src/main/resources/mule-simple-amqp-flow.xml", muleInboundOutboundXml)
-                .withApplicationProperties(applicationProperties)
-                .withBuildFileHavingDependencies(
-                        "org.springframework.boot:spring-boot-starter-web:2.5.5",
-                        "org.springframework.boot:spring-boot-starter-integration:2.5.5",
-                        "org.springframework.integration:spring-integration-amqp:5.4.4",
-                        "org.springframework.integration:spring-integration-stream:5.4.4",
-                        "org.springframework.integration:spring-integration-http:5.4.4"
-                )
-                .addRegistrar(registrar)
-                .build();
-        myAction.apply(projectContext);
+    public void generatesAmqpDSLStatementsAndConfigurations() {
+        addXMLFileToResource(muleInboundOutboundXml);
+        runAction();
         assertThat(projectContext.getProjectJavaSources().list()).hasSize(1);
+        List<SpringBootApplicationProperties> springBootApplicationProperties = projectContext.search(new SpringBootApplicationPropertiesResourceListFilter());
+        assertThat(springBootApplicationProperties).hasSize(1);
+
+        SpringBootApplicationProperties properties = springBootApplicationProperties.get(0);
+
+
+        String applicationPropertiesContent = properties.print();
+        assertThat(applicationPropertiesContent).isEqualTo(
+                "spring.rabbitmq.host=localhost\n" +
+                        "spring.rabbitmq.port=5672"
+        );
         assertThat(projectContext.getProjectJavaSources().list().get(0).print())
                 .isEqualTo(
                         "package com.example.javadsl;\n" +
@@ -131,59 +93,5 @@ public class MuleToJavaDSLMultipleTest extends JavaDSLActionBaseTest {
                                 "                .handle(Amqp.outboundAdapter(rabbitTemplate).exchangeName(\"sbm-integration-exchange\").routingKey(\"sbm-integration-queue-two\"))\n" +
                                 "                .get();\n" +
                                 "    }}");
-    }
-
-    @Test
-    public void generatesAMQPConnectorBean() {
-
-        ProjectContext projectContext = TestProjectContext.buildProjectContext(eventPublisher)
-                .addProjectResource("src/main/resources/mule-simple-amqp-flow.xml", muleInboundOutboundXml)
-                .withApplicationProperties(applicationProperties)
-                .withBuildFileHavingDependencies(
-                        "org.springframework.boot:spring-boot-starter-web:2.5.5",
-                        "org.springframework.boot:spring-boot-starter-integration:2.5.5",
-                        "org.springframework.integration:spring-integration-amqp:5.4.4",
-                        "org.springframework.integration:spring-integration-stream:5.4.4",
-                        "org.springframework.integration:spring-integration-http:5.4.4"
-                )
-                .addRegistrar(registrar)
-                .build();
-
-
-        myAction.apply(projectContext);
-
-        assertThat(projectContext.getProjectJavaSources().list()).hasSize(1);
-        List<SpringBootApplicationProperties> springBootApplicationProperties = projectContext.search(new SpringBootApplicationPropertiesResourceListFilter());
-        assertThat(springBootApplicationProperties).hasSize(1);
-
-        SpringBootApplicationProperties properties = springBootApplicationProperties.get(0);
-
-
-        String applicationPropertiesContent = properties.print();
-        assertThat(applicationPropertiesContent).isEqualTo(
-                "spring.rabbitmq.host=localhost\n" +
-                "spring.rabbitmq.port=5672"
-        );
-
-        assertThat(projectContext.getProjectJavaSources().list().get(0).print()).isEqualTo(
-                "package com.example.javadsl;\n" +
-                        "import org.springframework.amqp.rabbit.core.RabbitTemplate;\n" +
-                        "import org.springframework.context.annotation.Bean;\n" +
-                        "import org.springframework.context.annotation.Configuration;\n" +
-                        "import org.springframework.integration.amqp.dsl.Amqp;\n" +
-                        "import org.springframework.integration.dsl.IntegrationFlow;\n" +
-                        "import org.springframework.integration.dsl.IntegrationFlows;\n" +
-                        "import org.springframework.integration.handler.LoggingHandler;\n" +
-                        "\n" +
-                        "@Configuration\n" +
-                        "public class FlowConfigurations {\n" +
-                        "    @Bean\n" +
-                        "    IntegrationFlow amqp_muleFlow(org.springframework.amqp.rabbit.core.RabbitTemplate rabbitTemplate, org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {\n" +
-                        "        return IntegrationFlows.from(Amqp.inboundAdapter(connectionFactory, \"sbm-integration-queue-one\"))\n" +
-                        "                .log(LoggingHandler.Level.INFO, \"payload to be sent: #[new String(payload)]\")\n" +
-                        "                .handle(Amqp.outboundAdapter(rabbitTemplate).exchangeName(\"sbm-integration-exchange\").routingKey(\"sbm-integration-queue-two\"))\n" +
-                        "                .get();\n" +
-                        "    }}"
-        );
     }
 }
