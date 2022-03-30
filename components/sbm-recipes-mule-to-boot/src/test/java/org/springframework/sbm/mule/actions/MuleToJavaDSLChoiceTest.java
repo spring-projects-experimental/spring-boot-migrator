c106416573d3129c8a9596c7d7ce5e9a055a0b23
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-03-30 11:30:56 +0530

    Failing test for subflow calling with in choice

--- components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLChoiceTest.java
+++ components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLChoiceTest.java
@@ -62,6 +62,7 @@ public class MuleToJavaDSLChoiceTest extends JavaDSLActionBaseTest {
                         "import org.springframework.integration.dsl.IntegrationFlows;\n" +
                         "import org.springframework.integration.handler.LoggingHandler;\n" +
                         "import org.springframework.integration.http.dsl.Http;\n" +
+                        "import org.springframework.util.LinkedMultiValueMap;\n" +
                         "\n" +
                         "@Configuration\n" +
                         "public class FlowConfigurations {\n" +
@@ -92,6 +93,85 @@ public class MuleToJavaDSLChoiceTest extends JavaDSLActionBaseTest {
     @Test
     @Disabled
     public void whenExpressionCallsSubFlow() {
+        final String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "\n" +
+                "<mule xmlns:http=\"http://www.mulesoft.org/schema/mule/http\" xmlns:tracking=\"http://www.mulesoft.org/schema/mule/ee/tracking\" xmlns=\"http://www.mulesoft.org/schema/mule/core\" xmlns:doc=\"http://www.mulesoft.org/schema/mule/documentation\"\n" +
+                "    xmlns:spring=\"http://www.springframework.org/schema/beans\" \n" +
+                "    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+                "    xsi:schemaLocation=\"http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-current.xsd\n" +
+                "http://www.mulesoft.org/schema/mule/core http://www.mulesoft.org/schema/mule/core/current/mule.xsd\n" +
+                "http://www.mulesoft.org/schema/mule/http http://www.mulesoft.org/schema/mule/http/current/mule-http.xsd\n" +
+                "http://www.mulesoft.org/schema/mule/ee/tracking http://www.mulesoft.org/schema/mule/ee/tracking/current/mule-tracking-ee.xsd\">\n" +
+                "        <http:listener-config name=\"HTTP_Listener_Configuration\" host=\"0.0.0.0\" port=\"9081\" doc:name=\"HTTP Listener Configuration\"/>\n" +
+                "    <flow name=\"choiceFlow\">\n" +
+                "        <http:listener config-ref=\"HTTP_Listener_Configuration\" path=\"/choice\" doc:name=\"HTTP\"/>\n" +
+                "        <expression-filter expression=\"#[message.inboundProperties.'http.request.uri' != '/favicon.ico']\" doc:name=\"Expression\"/>\n" +
+                "        <set-variable variableName=\"language\" value=\"#[message.inboundProperties.'http.query.params'.language]\" doc:name=\"Set Language Variable\"/>\n" +
+                "        <choice doc:name=\"Choice\">\n" +
+                "            <when expression=\"#[flowVars.language == 'Spanish']\">\n" +
+                "                <flow-ref name=\"spanishHello\"></flow-ref>\n" +
+                "            </when>\n" +
+                "            <when expression=\"#[flowVars.language == 'French']\">\n" +
+                "                <set-payload doc:name=\"Reply in French\" value=\"Bonjour!\"/>\n" +
+                "            </when>\n" +
+                "            <otherwise>\n" +
+                "                <set-payload doc:name=\"Reply in English\" value=\"Hello\"/>\n" +
+                "            </otherwise>\n" +
+                "        </choice>\n" +
+                "        <logger message=\"#[payload]\" level=\"INFO\" doc:name=\"Logger\"/>\n" +
+                "    </flow>\n" +
+                "    <sub-flow name=\"spanishHello\">\n" +
+                "       <logger message=\"A spanish Hello\"\n" +
+                "             level=\"INFO\"\n" +
+                "             doc:name=\"RequestPayloadReceived\" category=\"${api.name}\" />\n" +
+                "       <set-payload doc:name=\"Reply in Spanish\" value=\"Hola!!!\"/>\n" +
+                "   </sub-flow>\n" +
+                "</mule>";
+        addXMLFileToResource(xml);
+        runAction();
+
+        assertThat(projectContext.getProjectJavaSources().list().get(0).print())
+                .isEqualTo("package com.example.javadsl;\n" +
+                        "\n" +
+                        "import org.springframework.context.annotation.Bean;\n" +
+                        "import org.springframework.context.annotation.Configuration;\n" +
+                        "import org.springframework.integration.dsl.IntegrationFlow;\n" +
+                        "import org.springframework.integration.dsl.IntegrationFlows;\n" +
+                        "import org.springframework.integration.handler.LoggingHandler;\n" +
+                        "import org.springframework.integration.http.dsl.Http;\n" +
+                        "import org.springframework.util.LinkedMultiValueMap;\n" +
+                        "\n" +
+                        "@Configuration\n" +
+                        "public class FlowConfigurations {\n" +
+                        "    @Bean\n" +
+                        "    IntegrationFlow choiceFlow(org.springframework.integration.dsl.IntegrationFlow spanishHello) {\n" +
+                        "        return IntegrationFlows.from(Http.inboundChannelAdapter(\"/choice\")).handle((p, h) -> p)\n" +
+                        "                //FIXME: element is not supported for conversion: <expression-filter/>\n" +
+                        "                //FIXME: element is not supported for conversion: <set-variable/>\n" +
+                        "                /*\n" +
+                        "                 * TODO: LinkedMultiValueMap might not be apt, substitute with right input type*/\n" +
+                        "                .<LinkedMultiValueMap<String, String>, String>route(\n" +
+                        "                        p -> p.getFirst(\"language\") /*TODO: use apt condition*/,\n" +
+                        "                        m -> m\n" +
+                        "                                .subFlowMapping(\"Spanish\" /*TODO: Translate dataValue to #[flowVars.language == 'Spanish']*/,\n" +
+                        "                                        sf -> sf.gateway(spanishHello)\n" +
+                        "                                )\n" +
+                        "                                .subFlowMapping(\"French\" /*TODO: Translate dataValue to #[flowVars.language == 'French']*/,\n" +
+                        "                                        sf -> sf.handle((p, h) -> \"Bonjour!\")\n" +
+                        "                                )\n" +
+                        "                                .resolutionRequired(false)\n" +
+                        "                                .defaultSubFlowMapping(sf -> sf.handle((p, h) -> \"Hello\"))\n" +
+                        "                )\n" +
+                        "                .log(LoggingHandler.Level.INFO, \"${payload}\")\n" +
+                        "                .get();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    @Bean\n" +
+                        "    IntegrationFlow spanishHello() {\n" +
+                        "        return flow -> flow\n" +
+                        "                .log(LoggingHandler.Level.INFO,  \"A spanish Hello\")\n" +
+                        "                .handle((p, h) -> \"Hola!!!\");\n" +
+                        "    }}");
     }
 
     @Test
@@ -102,6 +182,12 @@ public class MuleToJavaDSLChoiceTest extends JavaDSLActionBaseTest {
     @Test
     @Disabled
     public void choiceInsideAChoiceInsideAChoiceInsideAChoiceChoice() {
+
+    }
+
+    @Test
+    @Disabled
+    public void choiceComponentUsesSpringBeansExampleAMQP() {
 
     }
 }
