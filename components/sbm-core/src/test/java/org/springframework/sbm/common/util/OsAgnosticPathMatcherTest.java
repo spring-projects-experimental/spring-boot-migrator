cc87884fe2cff1c54324f4a8a6f25314bc04d4aa
Fabian Krüger <fkrueger@vmware.com>
2022-04-07 01:20:30 +0200

    Fix copyright header

--- components/sbm-core/src/test/java/org/springframework/sbm/common/util/OsAgnosticPathMatcherTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/common/util/OsAgnosticPathMatcherTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2021 - 2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.sbm.common.util;
 
 import org.junit.jupiter.params.ParameterizedTest;
