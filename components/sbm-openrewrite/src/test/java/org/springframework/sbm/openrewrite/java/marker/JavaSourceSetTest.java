6a98166e44ddd1fdb1ecb04c594262b03582c00b
Fabian Krüger <fkrueger@vmware.com>
2022-04-14 10:27:56 +0200

    Upgraded to 7.21.3. Creating empty JavaSourceSet fails with NPE

---
+++ components/sbm-openrewrite/src/test/java/org/springframework/sbm/openrewrite/java/marker/JavaSourceSetTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.sbm.openrewrite.java.marker;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.internal.JavaTypeCache;
+import org.openrewrite.java.marker.JavaSourceSet;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class JavaSourceSetTest {
+    @Test
+    void buildJavaSourceSet() {
+        Collection<Path> dependencies =  new ArrayList<>();
+        JavaTypeCache typeCache = new JavaTypeCache();
+        JavaSourceSet mainProvenance = JavaSourceSet.build("main", dependencies,  typeCache, true);
+    }
+}
