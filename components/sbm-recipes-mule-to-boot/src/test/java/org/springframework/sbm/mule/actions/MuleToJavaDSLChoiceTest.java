cd69bc95ae3579f24a474385a6ff3468a20a334c
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-04-27 10:00:54 +0530

    Removing unnecessary import

--- components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLChoiceTest.java
+++ components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLChoiceTest.java
@@ -15,7 +15,6 @@
  */
 package org.springframework.sbm.mule.actions;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
