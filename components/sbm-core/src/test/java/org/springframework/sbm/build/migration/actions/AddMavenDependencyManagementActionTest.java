06f9c83ff603c8a731d9fc0d531e0388c19ac436
Fabian Krüger <fkrueger@vmware.com>
2022-04-29 11:17:22 +0200

    Fix formatting in test

--- components/sbm-core/src/test/java/org/springframework/sbm/build/migration/actions/AddMavenDependencyManagementActionTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/build/migration/actions/AddMavenDependencyManagementActionTest.java
@@ -40,9 +40,9 @@ class AddMavenDependencyManagementActionTest {
                         "</project>\n";
 
         String expected =
-                "<?xml  version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                         "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-                        "    xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">\n" +
+                        "        xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">\n" +
                         "    <modelVersion>4.0.0</modelVersion>\n" +
                         "    <groupId>org.springframework.sbm.examples</groupId>\n" +
                         "    <artifactId>artifact-id</artifactId>\n" +
