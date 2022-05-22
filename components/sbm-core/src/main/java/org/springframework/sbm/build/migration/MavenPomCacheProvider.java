686dcf7183762dc6ddd028a19f3dd1b42fa0a2ba
Fabian Krüger <fkrueger@vmware.com>
2022-05-22 19:33:00 +0200

    Switch Rewrite cache from RocksDB to in Memory as concurrent access was failing in tests

--- components/sbm-core/src/main/java/org/springframework/sbm/build/migration/MavenPomCacheProvider.java
+++ components/sbm-core/src/main/java/org/springframework/sbm/build/migration/MavenPomCacheProvider.java
@@ -34,7 +34,7 @@ public class MavenPomCacheProvider {
 
     @PostConstruct
     void postConstruct() {
-        pomCache = rocksdb(); // mapdb();
+        pomCache = inMemory(); //rocksdb(); // mapdb();
     }
 
     public MavenPomCache getPomCache() {
