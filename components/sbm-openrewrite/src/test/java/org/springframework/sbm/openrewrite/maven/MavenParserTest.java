5a7ad80396644b57635457aabebda8d15e759c64
Fabian Krüger <fkrueger@vmware.com>
2022-05-25 11:10:10 +0200

    Moved MavenParserTest

--- components/sbm-openrewrite/src/test/java/org/springframework/sbm/openrewrite/maven/MavenParserTest.java
+++ components/sbm-openrewrite/src/test/java/org/springframework/sbm/openrewrite/maven/MavenParserTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.sbm.openrewrite;
+package org.springframework.sbm.openrewrite.maven;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
