4f163c211fad9d78fdc13333db90f4344e30130d
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-05-30 15:48:08 +0100

    Passing Test

--- components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLTransactionalTest.java
+++ components/sbm-recipes-mule-to-boot/src/test/java/org/springframework/sbm/mule/actions/MuleToJavaDSLTransactionalTest.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.sbm.mule.actions;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
