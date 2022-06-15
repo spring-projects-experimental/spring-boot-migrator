d28872f19d8b45ee23ad8605f2fda472251b6dc6
Fabian Krüger <fkrueger@vmware.com>
2022-06-15 17:26:32 +0200

    wip

---
+++ components/sbm-openrewrite/src/main/java/org/openrewrite/maven/AddMavenRepository.java
@@ -0,0 +1,106 @@
+package org.openrewrite.maven;
+
+import org.jetbrains.annotations.NotNull;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.maven.tree.Maven;
+import org.openrewrite.xml.AddToTagVisitor;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Xml;
+import org.springframework.sbm.build.api.RepositoryDefinition;
+
+import java.util.Optional;
+
+public class AddMavenRepository extends Recipe {
+
+    private RepositoryDefinition mavenRepository;
+
+    public AddMavenRepository(RepositoryDefinition repository) {
+        this.mavenRepository = repository;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Add a repository to Maven build file";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new AddRepositoryVisitor();
+    }
+
+    private class AddRepositoryVisitor extends MavenVisitor {
+        private AddRepositoryVisitor() {
+        }
+
+        public Maven visitMaven(Maven maven, ExecutionContext ctx) {
+            Xml.Tag parent = maven.getRoot();
+            Optional<Xml.Tag> repositoriesTag = parent.getChild("repositories");
+            if (repositoriesTag.isEmpty()) {
+                addRepositoriesTag(parent);
+            } else if(noRepositoryWithSameIdExists(repositoriesTag.get())){
+                addRepositoryTag(repositoriesTag.get());
+            }
+            return super.visitMaven(maven, ctx);
+        }
+
+        private boolean noRepositoryWithSameIdExists(Xml.Tag t) {
+            return t.getChildren().stream().anyMatch(repo -> repo.getChildren().stream().anyMatch(c -> c.getName().equals("id") && false == c.getValue().get().equals(mavenRepository.getId())));
+        }
+
+        private Xml.Tag addRepositoriesTag(Xml.Tag parent) {
+            Xml.Tag repositoriesTag = Xml.Tag.build(
+                            "<repositories>\n" +
+                            renderRepositoryTag() +
+                            "</repositories>\n");
+            this.doAfterVisit(new AddToTagVisitor(parent, repositoriesTag, new MavenTagInsertionComparator(parent.getChildren())));
+            return repositoriesTag;
+        }
+
+        private void addRepositoryTag(Xml.Tag parent) {
+            AddToTagVisitor visitor = new AddToTagVisitor(parent, Xml.Tag.build(renderRepositoryTag()));
+            this.doAfterVisit(visitor);
+        }
+
+        private String renderRepositoryTag() {
+            StringBuilder sb = new StringBuilder();
+                sb.append("<repository>\n");
+                sb.append("<id>" + mavenRepository.getId() + "</id>\n");
+                if(mavenRepository.getName() != null) {
+                    sb.append("<name>").append(mavenRepository.getName()).append("</name>\n");
+                }
+                if(mavenRepository.getUrl() != null) {
+                    sb.append("<url>").append(mavenRepository.getUrl()).append("</url>\n");
+                }
+                if(mavenRepository.getReleasesEnabled() != null && mavenRepository.getReleasesEnabled() == true) {
+                    String releaseSection = renderSection("releases", mavenRepository.getReleasesChecksumPolicy(), mavenRepository.getReleasesUpdatePolicy());
+                    sb.append(releaseSection);
+                }
+                if(mavenRepository.getSnapshotsEnabled() != null && mavenRepository.getSnapshotsEnabled() == true) {
+                    String snapshotsSection = renderSection("snapshots", mavenRepository.getSnapshotsChecksumPolicy(), mavenRepository.getSnapShotsUpdatePolicy());
+                    sb.append(snapshotsSection);
+                }
+                sb.append("</repository>\n");
+            return sb.toString();
+        }
+
+        @NotNull
+        private String renderSection(String type, String checksum, String update) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("<").append(type).append(">\n");
+            sb.append("<enabled>true</enabled>\n");
+            if (checksum != null) {
+                sb.append("<checksumPolicy>").append(checksum).append("</checksumPolicy>\n");
+            }
+            if (update != null) {
+                sb.append("<updatePolicy>").append(update).append("</updatePolicy>\n");
+            }
+            sb.append("</").append(type).append(">\n");
+            return sb.toString();
+        }
+
+    }
+
+}
---
+++ components/sbm-openrewrite/src/main/java/org/springframework/sbm/build/api/RepositoryDefinition.java
@@ -0,0 +1,21 @@
+
+package org.springframework.sbm.build.api;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RepositoryDefinition {
+
+    private String id;
+    private String url;
+    private String name;
+    private String layout;
+    private Boolean snapshotsEnabled;
+    private String snapshotsChecksumPolicy;
+    private String snapShotsUpdatePolicy;
+    private Boolean releasesEnabled;
+    private String releasesChecksumPolicy;
+    private String releasesUpdatePolicy;
+}
