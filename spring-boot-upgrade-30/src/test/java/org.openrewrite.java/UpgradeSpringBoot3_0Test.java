8a11913afb0b4a15300295c8e98ac0a017644549
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-06-20 15:03:36 +0100

    Refactor: adding repository and updating dependencies as separate tests

--- spring-boot-upgrade-30/src/test/java/org.openrewrite.java/UpgradeSpringBoot3_0Test.java
+++ spring-boot-upgrade-30/src/test/java/org.openrewrite.java/UpgradeSpringBoot3_0Test.java
@@ -1,6 +1,7 @@
 package org.openrewrite.java;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Result;
 import org.openrewrite.maven.MavenParser;
 import org.openrewrite.maven.UpgradeDependencyVersion;
@@ -28,13 +29,6 @@ public class UpgradeSpringBoot3_0Test {
                         "    <artifactId>spring-boot-upgrades-30</artifactId>\n" +
                         "    <version>0.1.0-SNAPSHOT</version>\n" +
                         "    <packaging>jar</packaging>\n" +
-                        " <repositories>\n" +
-                        "  <repository>\n" +
-                        "    <id>repository.spring.snapshot</id>\n" +
-                        "    <name>repository.spring.snapshot</name>\n" +
-                        "    <url>https://repo.spring.io/milestone</url>\n" +
-                        "  </repository>\n" +
-                        " </repositories>\n" +
                         "</project>";
 
         String expectedPom = "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
@@ -42,32 +36,82 @@ public class UpgradeSpringBoot3_0Test {
                 "    <parent>\n" +
                 "        <groupId>org.springframework.boot</groupId>\n" +
                 "        <artifactId>spring-boot-starter-parent</artifactId>\n" +
-                "        <version>3.0.0-M3</version>\n" +
+                "        <version>2.7.0</version>\n" +
                 "    </parent>\n" +
                 "    <groupId>org.springframework.sbm</groupId>\n" +
                 "    <artifactId>spring-boot-upgrades-30</artifactId>\n" +
                 "    <version>0.1.0-SNAPSHOT</version>\n" +
                 "    <packaging>jar</packaging>\n" +
-                " <repositories>\n" +
-                "  <repository>\n" +
-                "    <id>repository.spring.snapshot</id>\n" +
-                "    <name>repository.spring.snapshot</name>\n" +
-                "    <url>https://repo.spring.io/milestone</url>\n" +
-                "  </repository>\n" +
-                " </repositories>\n" +
+                "    <repositories>\n" +
+                "        <repository>\n" +
+                "            <id>repository.spring.milestone</id>\n" +
+                "            <name>repository.spring.milestone</name>\n" +
+                "            <url>https://repo.spring.io/milestone</url>\n" +
+                "        </repository>\n" +
+                "    </repositories>\n" +
                 "</project>";
 
-        List<Xml.Document> parse = MavenParser.builder().build().parse(pomXml);
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
+        List<Xml.Document> parse = MavenParser.builder().build().parse(ctx, pomXml);
 
         String recipeName = "org.openrewrite.java.spring.boot3.data.TEST";
+
         List<Result> result = RewriteTest
                 .fromRuntimeClasspath(recipeName)
-                .run(parse);
+                .run(parse, ctx);
 
         assertThat(result).hasSize(1);
         assertThat(result.get(0).getAfter().printAll()).isEqualTo(expectedPom);
     }
 
+    @Test
+    public void canAddRepository() {
+        String pomXml =
+                "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+                        "    <modelVersion>4.0.0</modelVersion>\n" +
+                        "    <parent>\n" +
+                        "        <groupId>org.springframework.boot</groupId>\n" +
+                        "        <artifactId>spring-boot-starter-parent</artifactId>\n" +
+                        "        <version>2.7.0</version>\n" +
+                        "    </parent>\n" +
+                        "    <groupId>org.springframework.sbm</groupId>\n" +
+                        "    <artifactId>spring-boot-upgrades-30</artifactId>\n" +
+                        "    <version>0.1.0-SNAPSHOT</version>\n" +
+                        "    <packaging>jar</packaging>\n" +
+                        "</project>";
+
+        String expectedPom = "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+                "    <modelVersion>4.0.0</modelVersion>\n" +
+                "    <parent>\n" +
+                "        <groupId>org.springframework.boot</groupId>\n" +
+                "        <artifactId>spring-boot-starter-parent</artifactId>\n" +
+                "        <version>2.7.0</version>\n" +
+                "    </parent>\n" +
+                "    <groupId>org.springframework.sbm</groupId>\n" +
+                "    <artifactId>spring-boot-upgrades-30</artifactId>\n" +
+                "    <version>0.1.0-SNAPSHOT</version>\n" +
+                "    <packaging>jar</packaging>\n" +
+                "    <repositories>\n" +
+                "        <repository>\n" +
+                "            <id>repository.spring.milestone</id>\n" +
+                "            <name>repository.spring.milestone</name>\n" +
+                "            <url>https://repo.spring.io/milestone</url>\n" +
+                "        </repository>\n" +
+                "    </repositories>\n" +
+                "</project>";
+
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
+        List<Xml.Document> parse = MavenParser.builder().build().parse(ctx, pomXml);
+
+        String recipeName = "org.openrewrite.java.spring.boot3.data.TEST2";
+
+        List<Result> result = RewriteTest
+                .fromRuntimeClasspath(recipeName)
+                .run(parse, ctx);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getAfter().printAll()).isEqualTo(expectedPom);
+    }
     @Test
     void testBumpingParentPomVersion() {
         String pomXml =
