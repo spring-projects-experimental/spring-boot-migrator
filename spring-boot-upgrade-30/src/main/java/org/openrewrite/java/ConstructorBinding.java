ebd8bc1e86678ba937774a59fa17ca87137dba7d
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-06-21 10:04:15 +0100

    Failing Test

---
+++ spring-boot-upgrade-30/src/main/java/org/openrewrite/java/ConstructorBinding.java
@@ -0,0 +1,10 @@
+package org.openrewrite.java;
+
+import org.openrewrite.Recipe;
+
+public class ConstructorBinding extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return "fdskjhgkfjdhgf fkjdh kdhfjklsdhf l;";
+    }
+}
---
+++ spring-boot-upgrade-30/src/test/java/org.openrewrite.java/ConstructorBindingTest.java
@@ -0,0 +1,62 @@
+package org.openrewrite.java;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Result;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Brain dump
+ * TODO:
+ *  * Source can be 11 and destination must be 17 separate test for that ?
+ * */
+public class ConstructorBindingTest {
+
+    @Test
+    void newGeneratedClassRemovesConstructorBinding() {
+
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
+
+        String source = "@ConfigurationProperties(prefix = \"mail\")\n" +
+                "@ConstructorBinding\n" +
+                "public class ConfigProperties {\n" +
+                "    private String hostName;\n" +
+                "    private int port;\n" +
+                "    private String from;\n" +
+                "\n" +
+                "    public ConfigProperties(String hostName, int port, String from) {\n" +
+                "        this.hostName = hostName;\n" +
+                "        this.port = port;\n" +
+                "        this.from = from;\n" +
+                "    }\n" +
+                "\n" +
+                "\n" +
+                "    public String getHostName() {\n" +
+                "        return hostName;\n" +
+                "    }\n" +
+                "\n" +
+                "    public int getPort() {\n" +
+                "        return port;\n" +
+                "    }\n" +
+                "\n" +
+                "    public String getFrom() {\n" +
+                "        return from;\n" +
+                "    }\n" +
+                "}";
+
+        List<J.CompilationUnit> parse = Java17Parser.builder().build().parse(ctx, source);
+
+        String recipeName = "org.openrewrite.java.spring.boot3.data.java.constructorbinding";
+
+        List<Result> result = RewriteTest
+                .fromRuntimeClasspath(recipeName)
+                .run(parse, ctx);
+
+        assertThat(result).hasSize(1);
+    }
+}
