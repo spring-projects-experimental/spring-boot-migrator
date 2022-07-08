38a2f0e0a22657eee40488e08a54a55ec1475146
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-07-08 12:57:14 +0200

    Refactored properties update test

--- spring-boot-upgrade-30/src/test/java/org/openrewrite/java/spring/boot3/UpdatePropertyTest.java
+++ spring-boot-upgrade-30/src/test/java/org/openrewrite/java/spring/boot3/UpdatePropertyTest.java
@@ -1,9 +1,12 @@
 package org.openrewrite.java.spring.boot3;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Result;
 import org.openrewrite.properties.PropertiesParser;
@@ -19,29 +22,40 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 public class UpdatePropertyTest {
 
     private final InMemoryExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
 
     @ParameterizedTest
-    @MethodSource("provideYamlIO")
-    void runYamlTests(String input, String expected) {
+    @ValueSource(strings = {
+            "/spring-3_0-config-refactor-yaml-data.txt",
+            "/spring-3_0-config-refactor-yaml-datasource.txt",
+            "/spring-3_0-config-refactor-yaml-elasticsearch.txt",
+            "/spring-3_0-config-refactor-yaml-misc.txt"})
+    void runYamlTestsData(String inputFilePath) throws IOException {
 
-        List<Result> result = runRecipeOnYml(input);
+        Pair<String, String> testData = provideIO(inputFilePath);
+        List<Result> result = runRecipeOnYml(testData.getLeft());
 
         assertThat(result).hasSize(1);
-        assertThat(result.get(0).getAfter().printAll()).isEqualTo(expected);
+        assertThat(result.get(0).getAfter().printAll()).isEqualTo(testData.getRight());
     }
 
     @ParameterizedTest
-    @MethodSource("providePropIO")
-    void runPropertiesTest(String input, String expected) {
+    @ValueSource(strings = {
+            "/spring-3_0-config-refactor-properties-data.txt",
+            "/spring-3_0-config-refactor-properties-datasource.txt",
+            "/spring-3_0-config-refactor-properties-elasticsearch.txt",
+            "/spring-3_0-config-refactor-properties-misc.txt"})
+    void runYamlTestsDataSource(String inputFilePath) throws IOException {
 
-        List<Result> result = runRecipeOnProperties(input);
+        Pair<String, String> testData = provideIO(inputFilePath);
+        List<Result> result = runRecipeOnProperties(testData.getLeft());
 
         assertThat(result).hasSize(1);
-        assertThat(result.get(0).getAfter().printAll()).isEqualTo(expected);
+        assertThat(result.get(0).getAfter().printAll()).isEqualTo(testData.getRight());
     }
 
     private List<Result> runRecipeOnProperties(@Language("properties") String source) {
@@ -63,40 +77,17 @@ public class UpdatePropertyTest {
                 .run(document, ctx);
     }
 
-    private static Stream<Arguments> provideYamlIO() throws IOException {
+    private static Pair<String, String> provideIO(String inputFilePath) throws IOException {
         InputStream data =
-                UpdatePropertyTest.class.getResourceAsStream("/spring-3_0-config-refactor-yaml.txt");
+                UpdatePropertyTest.class.getResourceAsStream(inputFilePath);
 
         if (data == null) {
-            throw new RuntimeException("unable to read /spring-3_0-config-refactor-yaml.txt file");
+            throw new RuntimeException("unable to read: "+ inputFilePath);
         }
 
         String fileContent = new String(data.readAllBytes());
-        String[] tests = fileContent.split("--- end of test ---");
+        String[] k = fileContent.split("expected:.*\n");
 
-        return Arrays.stream(tests)
-                .map(test -> {
-                    String[] k = test.split("expected:.*\n");
-                    return Arguments.of(k[0].replaceAll("input:.*\n", ""), k[1]);
-                });
-    }
-
-    private static Stream<Arguments> providePropIO() throws IOException {
-        String inputFile = "/spring-3_0-config-refactor-properties.txt";
-        InputStream data =
-                UpdatePropertyTest.class.getResourceAsStream(inputFile);
-
-        if (data == null) {
-            throw new RuntimeException(inputFile);
-        }
-
-        String fileContent = new String(data.readAllBytes());
-        String[] tests = fileContent.split("--- end of test ---");
-
-        return Arrays.stream(tests)
-                .map(test -> {
-                    String[] k = test.split("expected:.*\n");
-                    return Arguments.of(k[0].replaceAll("input:.*\n", ""), k[1]);
-                });
+        return new ImmutablePair<>(k[0].replaceAll("input:.*\n", ""), k[1]);
     }
 }
