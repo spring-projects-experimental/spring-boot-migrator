df43ab8d8a584bc9d57e629fc826d263dc7d3cc7
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-07-13 15:21:23 +0100

    assert artifactId and groupID

--- applications/spring-shell/src/test/java/org/springframework/sbm/BootUpgrade_27_30_IntegrationTest.java
+++ applications/spring-shell/src/test/java/org/springframework/sbm/BootUpgrade_27_30_IntegrationTest.java
@@ -1,11 +1,13 @@
 package org.springframework.sbm;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.maven.MavenParser;
 import org.openrewrite.xml.tree.Xml;
 import org.springframework.sbm.IntegrationTestBaseClass;
 
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -27,15 +29,25 @@ public class BootUpgrade_27_30_IntegrationTest extends IntegrationTestBaseClass 
 
         String pomContent = loadFile(Path.of("pom.xml"));
 
-        MavenParser mavenParser = new MavenParser.Builder().build();
-        List<Xml.Document> mavenAsXMLDocuments = mavenParser.parse(pomContent);
-        String version = mavenAsXMLDocuments.get(0)
+        Xml.Document mavenAsXMLDocument = parsePom(pomContent);
+
+        Xml.Tag parentTag =mavenAsXMLDocument
                 .getRoot()
-                .getChildren("parent")
-                .get(0)
-                .getChildValue("version")
-                .get();
+                .getChildren("parent").get(0);
+
+        String version = parentTag.getChildValue("version").get();
+
+        String groupId = parentTag.getChildValue("groupId").get();
+        String artifactId = parentTag.getChildValue("artifactId").get();
 
         assertThat(version).isEqualTo("3.0.0-M3");
+        assertThat(groupId).isEqualTo("org.springframework.boot");
+        assertThat(artifactId).isEqualTo("spring-boot-starter-parent");
+    }
+
+    @NotNull
+    private Xml.Document parsePom(String pomContent) {
+        MavenParser mavenParser = new MavenParser.Builder().build();
+        return mavenParser.parse(pomContent).get(0);
     }
 }
