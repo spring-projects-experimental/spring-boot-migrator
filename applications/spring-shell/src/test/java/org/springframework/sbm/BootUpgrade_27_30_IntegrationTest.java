632c38ef3169d0321e0fcd16243d6efadd06cb94
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-07-13 14:32:08 +0100

    Assert if version is of right number

--- applications/spring-shell/src/test/java/org/springframework/sbm/BootUpgrade_27_30_IntegrationTest.java
+++ applications/spring-shell/src/test/java/org/springframework/sbm/BootUpgrade_27_30_IntegrationTest.java
@@ -1,9 +1,14 @@
 package org.springframework.sbm;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.maven.MavenParser;
+import org.openrewrite.xml.tree.Xml;
 import org.springframework.sbm.IntegrationTestBaseClass;
 
 import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BootUpgrade_27_30_IntegrationTest extends IntegrationTestBaseClass {
 
@@ -21,6 +26,16 @@ public class BootUpgrade_27_30_IntegrationTest extends IntegrationTestBaseClass 
         applyRecipe("boot-2.7-3.0-dependency-version-update");
 
         String pomContent = loadFile(Path.of("pom.xml"));
-        System.out.println(pomContent);
+
+        MavenParser mavenParser = new MavenParser.Builder().build();
+        List<Xml.Document> mavenAsXMLDocuments = mavenParser.parse(pomContent);
+        String version = mavenAsXMLDocuments.get(0)
+                .getRoot()
+                .getChildren("parent")
+                .get(0)
+                .getChildValue("version")
+                .get();
+
+        assertThat(version).isEqualTo("3.0.0-M3");
     }
 }
