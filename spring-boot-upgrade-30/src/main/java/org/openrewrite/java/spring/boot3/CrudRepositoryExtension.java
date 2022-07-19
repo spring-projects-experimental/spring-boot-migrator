b56c99bcd6f5992aaff831aba04fe533d12550b8
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-07-19 11:41:07 +0100

    Moving condition to apply recipe or not into getApplicableTests

--- spring-boot-upgrade-30/src/main/java/org/openrewrite/java/spring/boot3/CrudRepositoryExtension.java
+++ spring-boot-upgrade-30/src/main/java/org/openrewrite/java/spring/boot3/CrudRepositoryExtension.java
@@ -1,10 +1,12 @@
 package org.openrewrite.java.spring.boot3;
 
-import org.openrewrite.ExecutionContext;
-import org.openrewrite.Recipe;
+import org.jetbrains.annotations.NotNull;
+import org.openrewrite.*;
+import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
+
 import java.util.List;
 
 public class CrudRepositoryExtension extends Recipe {
@@ -14,10 +16,35 @@ public class CrudRepositoryExtension extends Recipe {
     }
 
     @Override
+    protected @Nullable TreeVisitor<?, ExecutionContext> getApplicableTest() {
+        return new JavaIsoVisitor<>() {
+            @Override
+            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
+                return doesItExtendPagingAndSorting(classDecl) ? applyThisRecipe(classDecl) : ceaseVisit(classDecl);
+            }
+
+            private boolean doesItExtendPagingAndSorting(J.ClassDeclaration classDecl) {
+                String className = classDecl.getImplements().get(0).getType().toString();
+                return className.equals("org.springframework.data.repository.PagingAndSortingRepository");
+            }
+
+            private J.ClassDeclaration ceaseVisit(J.ClassDeclaration classDecl) {
+                return classDecl;
+            }
+
+            @NotNull
+            private J.ClassDeclaration applyThisRecipe(J.ClassDeclaration classDecl) {
+                return classDecl.withMarkers(classDecl.getMarkers().searchResult());
+            }
+        };
+    }
+
+    @Override
     protected JavaIsoVisitor<ExecutionContext> getVisitor() {
         return new JavaIsoVisitor<>() {
             @Override
             public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
+
                 List<JavaType.FullyQualified> interfaces = classDecl.getType().getInterfaces();
                 List<JavaType> typeParameters = interfaces.get(0).getTypeParameters();
                 doAfterVisit(new ImplementTypedInterface(classDecl, "org.springframework.data.repository.CrudRepository", typeParameters));
