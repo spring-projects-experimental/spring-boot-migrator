4884fc3c323e1a13033aa097c84c9d2916a16bc7
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-07-20 13:10:01 +0200

    Moved Crud recipe and tests

---
+++ components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/CrudRepositoryExtensionTest.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2021 - 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.sbm.boot.upgrade_27_30;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+public class CrudRepositoryExtensionTest {
+
+    private JavaTestHelper javaTestHelper = new JavaTestHelper();
+    private String recipeName = "org.boot3.Crud";
+
+    @Test
+    public void shouldAddCrudRepository() {
+
+        javaTestHelper.runAndVerify(
+                recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface PagingAndSortingRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                public interface A extends PagingAndSortingRepository<String, Long> {
+                }
+                """,
+                """
+                package test;
+                import org.springframework.data.repository.CrudRepository;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                
+                public interface A extends PagingAndSortingRepository<String, Long>, CrudRepository<String, Long> {
+                }
+                """
+        );
+    }
+
+    @Test
+    public void canDoQuestionMark() {
+
+        javaTestHelper.runAndVerify(recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface PagingAndSortingRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package test;
+                        public interface Payment<T> {
+                            T hello();
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                public interface A extends PagingAndSortingRepository<Payment<?>, Long> {
+                }
+                """,
+                """
+                package test;
+                import org.springframework.data.repository.CrudRepository;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                
+                public interface A extends PagingAndSortingRepository<Payment<?>, Long>, CrudRepository<Payment<?>, Long> {
+                }
+                """
+                );
+    }
+
+    @Test
+    public void onlyExtendCrudRepoIfInterfaceHasPagingAndSortingRepository() {
+        javaTestHelper.runAndVerify(recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface HelloWorld<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package test;
+                        public interface Payment<T> {
+                            T hello();
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.HelloWorld;
+                public interface A extends HelloWorld<Payment<?>, Long> {
+                }
+                """
+        );
+    }
+
+    @Test
+    public void whenThereAreNoParametersWhilstExtending() {
+
+        javaTestHelper.runAndVerify(
+                recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface PagingAndSortingRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                public interface A extends PagingAndSortingRepository {
+                }
+                """,
+                """
+                package test;
+                import org.springframework.data.repository.CrudRepository;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                
+                public interface A extends PagingAndSortingRepository, CrudRepository {
+                }
+                """
+        );
+    }
+
+    @Test
+    public void multipleExtends() {
+        javaTestHelper.runAndVerify(
+                recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface PagingAndSortingRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface Hello<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                import org.springframework.data.repository.Hello;
+                public interface A extends Hello<String, Long>, PagingAndSortingRepository {
+                }
+                """,
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                import org.springframework.data.repository.CrudRepository;
+                import org.springframework.data.repository.Hello;
+                
+                public interface A extends Hello<String, Long>, PagingAndSortingRepository, CrudRepository {
+                }
+                """
+        );
+    }
+
+    @Test
+    public void classImplementsPagingRepository() {
+        javaTestHelper.runAndVerify(
+                recipeName,
+                List.of("""
+                        package org.springframework.data.repository;
+                        public interface PagingAndSortingRepository<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface Hello<T, ID> {
+                        }
+                        """,
+                        """
+                        package org.springframework.data.repository;
+                        public interface CrudRepository<T, ID> {
+                        }
+                        """),
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                import org.springframework.data.repository.Hello;
+                public class A implements Hello<String, Long>, PagingAndSortingRepository<String, Long> {
+                }
+                """,
+                """
+                package test;
+                import org.springframework.data.repository.PagingAndSortingRepository;
+                import org.springframework.data.repository.CrudRepository;
+                import org.springframework.data.repository.Hello;
+                
+                public class A implements Hello<String, Long>, PagingAndSortingRepository<String, Long>, CrudRepository<String, Long> {
+                }
+                """
+        );
+    }
+}
---
+++ components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/JavaTestHelper.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 - 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.sbm.boot.upgrade_27_30;
+
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.annotations.NotNull;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.Result;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JavaTestHelper {
+
+    public void runAndVerify(
+            String recipeName,
+            List<String> dependsOn,
+            @Language("java") String before
+    ) {
+        List<Result> result = runRecipe(recipeName, dependsOn, before);
+        assertThat(result).hasSize(0);
+    }
+
+    public void runAndVerify(
+            String recipeName,
+            List<String> dependsOn,
+            @Language("java") String before,
+            @Language("java") String after
+            ) {
+        List<Result> result = runRecipe(recipeName, dependsOn, before);
+
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getAfter().printAll()).isEqualTo(after);
+    }
+
+    @NotNull
+    private List<Result> runRecipe(String recipeName, List<String> dependsOn, @Language("java") String before) {
+
+        List<Throwable> errors = new ArrayList<>();
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext((ex) -> {
+            ex.printStackTrace();
+            errors.add(ex);
+        });
+
+        JavaParser parser = JavaParser
+                .fromJavaVersion()
+                .dependsOn(dependsOn.toArray(new String[0]))
+                .build();
+
+        List<J.CompilationUnit> cu = parser.parse(before);
+
+        List<Result> result = RewriteTest
+                .fromRuntimeClasspath(recipeName)
+                .run(cu, ctx);
+
+        assertThat(errors).hasSize(0);
+        return result;
+    }
+}
