79e3c28f7ddb80e919faa62437772f21c15d4fce
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-07-28 16:39:57 +0100

    Integrating BOM upgrade recipe

--- components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/UpgradeBomTo30Test.java
+++ components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/UpgradeBomTo30Test.java
@@ -135,4 +135,68 @@ public class UpgradeBomTo30Test {
                         </project>
                         """);
     }
+
+    @Test
+    public void whenThereIsNoBomNoChanges() {
+        Recipe recipe = new UpgradeDependencyVersion(
+                "org.springframework.boot",
+                "spring-boot-dependencies",
+                "3.0.0-M3",
+                null,
+                null
+        );
+
+        List<Throwable> errors = new ArrayList<>();
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext((ex) -> {
+            ex.printStackTrace();
+            errors.add(ex);
+        });
+
+        MavenParser parser = MavenParser.builder().build();
+        List<Xml.Document> documentList = parser.parse("""
+                <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                                
+                    <groupId>test</groupId>
+                    <artifactId>test</artifactId>
+                    <version>1.0.0-SNAPSHOT</version>
+                                
+                    <properties>
+                        <spring.version>2.7.1</spring.version>
+                    </properties>
+                    <name>Test</name>
+                                
+                    <parent>
+                        <groupId>org.springframework.boot</groupId>
+                        <artifactId>spring-boot-starter-parent</artifactId>
+                        <version>3.0.0-M3</version>
+                    </parent>
+                    <repositories>
+                        <repository>
+                            <id>spring-milestone</id>
+                            <url>https://repo.spring.io/milestone</url>
+                            <snapshots>
+                                <enabled>false</enabled>
+                            </snapshots>
+                        </repository>
+                    </repositories>
+
+                    <pluginRepositories>
+                        <pluginRepository>
+                            <id>spring-milestone</id>
+                            <url>https://repo.spring.io/milestone</url>
+                            <snapshots>
+                                <enabled>false</enabled>
+                            </snapshots>
+                        </pluginRepository>
+                    </pluginRepositories>
+                </project>
+                """);
+
+        List<Result> result = recipe.run(documentList, ctx);
+
+        assertThat(result).hasSize(0);
+    }
 }
