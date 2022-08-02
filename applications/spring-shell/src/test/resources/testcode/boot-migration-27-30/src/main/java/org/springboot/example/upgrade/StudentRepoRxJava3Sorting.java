7d166153d207817276e7f51f1d95dc2798216dd2
ashakirin <2254222+ashakirin@users.noreply.github.com>
2022-08-02 13:15:47 +0200

    Fixed crud repo integration test (#279)

--- applications/spring-shell/src/test/resources/testcode/boot-migration-27-30/src/main/java/org/springboot/example/upgrade/StudentRepoRxJava3Sorting.java
+++ applications/spring-shell/src/test/resources/testcode/boot-migration-27-30/src/main/java/org/springboot/example/upgrade/StudentRepoRxJava3Sorting.java
@@ -2,5 +2,5 @@ package org.springboot.example.upgrade;
 
 import org.springframework.data.repository.reactive.RxJava3SortingRepository;
 
-public interface StudentRepoRxJava3Sorting extends RxJava3SortingRepository<Student<?>, Long> {
+public interface StudentRepoRxJava3Sorting extends RxJava3SortingRepository<Student, Long> {
 }
