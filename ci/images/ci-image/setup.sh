a3d3e1da8fb9ceb76368e3e9cd161f44b6379c14
Fabian Krüger <fkrueger@vmware.com>
2022-08-06 02:13:03 +0200

    using Spring Boot scripts

--- ci/images/ci-image/setup.sh
+++ ci/images/ci-image/setup.sh
@@ -40,14 +40,14 @@ fi
 ###########################################################
 # DOCKER
 ###########################################################
-cd /
-DOCKER_URL=$( ./get-docker-url.sh )
-curl -L ${DOCKER_URL} | tar zx
-mv /docker/* /bin/
-chmod +x /bin/docker*
-
-export ENTRYKIT_VERSION=0.4.0
-curl -L https://github.com/progrium/entrykit/releases/download/v${ENTRYKIT_VERSION}/entrykit_${ENTRYKIT_VERSION}_Linux_x86_64.tgz | tar zx
-chmod +x entrykit && \
-mv entrykit /bin/entrykit && \
-entrykit --symlink
+#cd /
+#DOCKER_URL=$( ./get-docker-url.sh )
+#curl -L ${DOCKER_URL} | tar zx
+#mv /docker/* /bin/
+#chmod +x /bin/docker*
+#
+#export ENTRYKIT_VERSION=0.4.0
+#curl -L https://github.com/progrium/entrykit/releases/download/v${ENTRYKIT_VERSION}/entrykit_${ENTRYKIT_VERSION}_Linux_x86_64.tgz | tar zx
+#chmod +x entrykit && \
+#mv entrykit /bin/entrykit && \
+#entrykit --symlink
