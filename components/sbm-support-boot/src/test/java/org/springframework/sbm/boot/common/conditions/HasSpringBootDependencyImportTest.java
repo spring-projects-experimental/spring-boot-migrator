709b8880712244c3a6f0fcc97beb20046bad3793
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-09-07 15:51:03 +0200

    Ping test for import dependencies condition

---
+++ components/sbm-support-boot/src/test/java/org/springframework/sbm/boot/common/conditions/HasSpringBootDependencyImportTest.java
@@ -0,0 +1,50 @@
+package org.springframework.sbm.boot.common.conditions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.sbm.boot.properties.SpringApplicationPropertiesPathMatcher;
+import org.springframework.sbm.boot.properties.SpringBootApplicationPropertiesRegistrar;
+import org.springframework.sbm.engine.context.ProjectContext;
+import org.springframework.sbm.project.resource.TestProjectContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HasSpringBootDependencyImportTest {
+
+    @Test
+    public void conditionShouldBeTrueForImport() {
+        ProjectContext projectContext = TestProjectContext.buildProjectContext()
+                .withMavenRootBuildFileSource("""
+                <?xml version="1.0" encoding="UTF-8"?>
+                <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                    <groupId>com.example</groupId>
+                    <artifactId>explicit-deps-app</artifactId>
+                    <version>0.0.1-SNAPSHOT</version>
+                    <name>explicit-deps-app</name>
+                    <description>explicit-deps-app</description>
+                    <dependencyManagement>
+                         <dependencies>
+                            <dependency>
+                                <groupId>org.springframework.boot</groupId>
+                                <artifactId>spring-boot-dependencies</artifactId>
+                                <version>2.7.0</version>
+                                <type>pom</type>
+                                <scope>import</scope>
+                            </dependency>
+                        </dependencies>
+                    </dependencyManagement>
+                </project>
+                        """)
+                .build();
+
+        HasSpringBootDependencyImport condition = new HasSpringBootDependencyImport();
+        condition.setVersionPattern("2\\.7\\..*");
+
+        boolean result = condition.evaluate(projectContext);
+
+        assertThat(result).isTrue();
+    }
+
+}
