625d7ebdfe2f79899321c326cc2868d75756583a
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-09-07 11:35:03 +0200

    Ping old SpringBoot version test

--- components/sbm-openrewrite/src/test/java/org/springframework/sbm/maven/UpgradeUnmanagedSpringProjectTest.java
+++ components/sbm-openrewrite/src/test/java/org/springframework/sbm/maven/UpgradeUnmanagedSpringProjectTest.java
@@ -179,4 +179,60 @@ public class UpgradeUnmanagedSpringProjectTest {
         assertThat(result).hasSize(0);
 
     }
+
+    @Test
+    void shouldNotUpdateBomForOldVersion() {
+        Recipe recipe = new UpgradeUnmanagedSpringProject("3.0.0-M3");
+
+        List<Throwable> errors = new ArrayList<>();
+        InMemoryExecutionContext ctx = new InMemoryExecutionContext((ex) -> {
+            ex.printStackTrace();
+            errors.add(ex);
+        });
+
+        MavenParser parser = MavenParser.builder().build();
+        List<Xml.Document> documentList = parser.parse("""
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>com.example</groupId>
+    <artifactId>explicit-deps-app</artifactId>
+    <version>0.0.1-SNAPSHOT</version>
+    <name>explicit-deps-app</name>
+    <description>explicit-deps-app</description>
+    <properties>
+        <java.version>17</java.version>
+        <maven.compiler.source>17</maven.compiler.source>
+        <maven.compiler.target>17</maven.compiler.target>
+    </properties>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+            <version>2.6.0</version>
+        </dependency>
+        <dependency>
+            <groupId>io.dropwizard.metrics</groupId>
+            <artifactId>metrics-annotation</artifactId>
+            <version>4.2.8</version>
+        </dependency>
+    </dependencies>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+</project>
+                """);
+
+        List<Result> result = recipe.run(documentList, ctx);
+
+        assertThat(result).hasSize(0);
+    }
 }
