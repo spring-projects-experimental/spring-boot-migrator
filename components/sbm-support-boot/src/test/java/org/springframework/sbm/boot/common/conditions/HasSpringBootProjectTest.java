e2645366363c42b603426f4ad6ad31b14c630ebc
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-09-08 14:51:08 +0530

    tests pass

--- components/sbm-support-boot/src/test/java/org/springframework/sbm/boot/common/conditions/HasSpringBootProjectTest.java
+++ components/sbm-support-boot/src/test/java/org/springframework/sbm/boot/common/conditions/HasSpringBootProjectTest.java
@@ -1,22 +1,17 @@
 package org.springframework.sbm.boot.common.conditions;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.sbm.engine.context.ProjectContext;
 import org.springframework.sbm.project.resource.TestProjectContext;
 
-import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,7 +30,12 @@ class HasSpringBootProjectTest {
     @ParameterizedTest
     @CsvSource(value = {
             //isParentManaged, dependencyManaged, manuallyManaged, expected
-            "true,             false,             false,           true"
+            "true,             false,             false,           true",
+            "false,            true,              false,           true",
+            "false,            false,             true,            true",
+            "true,             true,              true,            true",
+            "false,            false,             false,           false",
+
     })
     public void conditionShouldBeTrueIfOneIsTrue(boolean isParentManaged,
                                                  boolean dependencyManaged,
