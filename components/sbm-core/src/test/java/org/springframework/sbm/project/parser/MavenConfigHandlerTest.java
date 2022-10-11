ad5716671750f45061ad90713c9d60aeb93f9ceb
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-10-11 10:39:51 +0100

    Refactor

--- components/sbm-core/src/test/java/org/springframework/sbm/project/parser/MavenConfigHandlerTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/project/parser/MavenConfigHandlerTest.java
@@ -24,10 +24,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MavenConfigHandlerTest {
 
