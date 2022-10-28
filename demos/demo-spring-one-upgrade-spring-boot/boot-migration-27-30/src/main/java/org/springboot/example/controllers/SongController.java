62b74fe9b99a9d7356ad9574c10c29532144bdf7
sanagaraj-pivotal <sanagaraj@pivotal.io>
2022-10-28 17:08:11 +0100

    Refactor

--- demos/demo-spring-one-upgrade-spring-boot/boot-migration-27-30/src/main/java/org/springboot/example/controllers/SongController.java
+++ demos/demo-spring-one-upgrade-spring-boot/boot-migration-27-30/src/main/java/org/springboot/example/controllers/SongController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
