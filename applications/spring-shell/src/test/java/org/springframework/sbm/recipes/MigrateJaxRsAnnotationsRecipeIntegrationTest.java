39ea3ea1d9f877a540f3f73c37fe0c354e47b038
Vincent Botteman <vincent.botteman@gmail.com>
2022-11-13 20:07:50 +0100

    replace multi-line text with text block

--- applications/spring-shell/src/test/java/org/springframework/sbm/recipes/MigrateJaxRsAnnotationsRecipeIntegrationTest.java
+++ applications/spring-shell/src/test/java/org/springframework/sbm/recipes/MigrateJaxRsAnnotationsRecipeIntegrationTest.java
@@ -33,89 +33,93 @@ public class MigrateJaxRsAnnotationsRecipeIntegrationTest extends IntegrationTes
     }
 
     private final String expectedJavaSource =
-            "package com.example.jee.app;\n" +
-                    "\n" +
-                    "import org.springframework.http.MediaType;\n" +
-                    "import org.springframework.web.bind.annotation.PathVariable;\n" +
-                    "import org.springframework.web.bind.annotation.RequestMapping;\n" +
-                    "import org.springframework.web.bind.annotation.RequestMethod;\n" +
-                    "import org.springframework.web.bind.annotation.RestController;\n" +
-                    "\n" +
-                    "@RestController\n" +
-                    "@RequestMapping(value = \"/\")\n" +
-                    "public class PersonController {\n" +
-                    "\n" +
-                    "    @RequestMapping(value = \"/json/{name}\", produces = \"application/json\", consumes = \"application/json\", method = RequestMethod.POST)\n" +
-                    "    public String getHelloWorldJSON(@PathVariable(\"name\") String name) throws Exception {\n" +
-                    "        System.out.println(\"name: \" + name);\n" +
-                    "        return \"{\\\"Hello\\\":\\\"\" + name + \"\\\"\";\n" +
-                    "    }\n" +
-                    "\n" +
-                    "    @RequestMapping(value = \"/json\", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)\n" +
-                    "    public String getAllPersons() throws Exception {\n" +
-                    "        return \"{\\\"message\\\":\\\"No person here...\\\"\";\n" +
-                    "    }\n" +
-                    "\n" +
-                    "\n" +
-                    "    @RequestMapping(value = \"/xml/{name}\", produces = MediaType.APPLICATION_XML_VALUE, consumes = MediaType.APPLICATION_XML_VALUE, method = RequestMethod.POST)\n" +
-                    "    public String getHelloWorldXML(@PathVariable(\"name\") String name) throws Exception {\n" +
-                    "        System.out.println(\"name: \" + name);\n" +
-                    "        return \"<xml>Hello \"+name+\"</xml>\";\n" +
-                    "    }\n" +
-                    "\n" +
-                    "}\n";
+            """
+                    package com.example.jee.app;
+
+                    import org.springframework.http.MediaType;
+                    import org.springframework.web.bind.annotation.PathVariable;
+                    import org.springframework.web.bind.annotation.RequestMapping;
+                    import org.springframework.web.bind.annotation.RequestMethod;
+                    import org.springframework.web.bind.annotation.RestController;
+
+                    @RestController
+                    @RequestMapping(value = "/")
+                    public class PersonController {
+
+                        @RequestMapping(value = "/json/{name}", produces = "application/json", consumes = "application/json", method = RequestMethod.POST)
+                        public String getHelloWorldJSON(@PathVariable("name") String name) throws Exception {
+                            System.out.println("name: " + name);
+                            return "{\\"Hello\\":\\"" + name + "\\"";
+                        }
+
+                        @RequestMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+                        public String getAllPersons() throws Exception {
+                            return "{\\"message\\":\\"No person here...\\"";
+                        }
+
+
+                        @RequestMapping(value = "/xml/{name}", produces = MediaType.APPLICATION_XML_VALUE, consumes = MediaType.APPLICATION_XML_VALUE, method = RequestMethod.POST)
+                        public String getHelloWorldXML(@PathVariable("name") String name) throws Exception {
+                            System.out.println("name: " + name);
+                            return "<xml>Hello "+name+"</xml>";
+                        }
+
+                    }
+                    """;
 
     private final String expectedPomSource =
-            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                    "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-                    "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd\">\n" +
-                    "    <modelVersion>4.0.0</modelVersion>\n" +
-                    "    <groupId>org.springframework.sbm.examples</groupId>\n" +
-                    "    <artifactId>migrate-jax-rs</artifactId>\n" +
-                    "    <packaging>jar</packaging>\n" +
-                    "    <version>0.0.1-SNAPSHOT</version>\n" +
-                    "    <properties>\n" +
-                    "        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>\n" +
-                    "    </properties>\n" +
-                    "    <build>\n" +
-                    "        <plugins>\n" +
-                    "            <plugin>\n" +
-                    "                <groupId>org.apache.maven.plugins</groupId>\n" +
-                    "                <artifactId>maven-compiler-plugin</artifactId>\n" +
-                    "                <version>3.5.1</version>\n" +
-                    "                <configuration>\n" +
-                    "                    <source>1.8</source>\n" +
-                    "                    <target>1.8</target>\n" +
-                    "                </configuration>\n" +
-                    "            </plugin>\n" +
-                    "        </plugins>\n" +
-                    "    </build>\n" +
-                    "    <dependencies>\n" +
-                    "        <dependency>\n" +
-                    "            <groupId>org.springframework.boot</groupId>\n" +
-                    "            <artifactId>spring-boot-starter-web</artifactId>\n" +
-                    "            <version>2.3.4.RELEASE</version>\n" +
-                    "        </dependency>\n" +
-                    "        <dependency>\n" +
-                    "            <groupId>org.jboss.spec.javax.ws.rs</groupId>\n" +
-                    "            <artifactId>jboss-jaxrs-api_2.1_spec</artifactId>\n" +
-                    "            <version>1.0.1.Final</version>\n" +
-                    "            <scope>runtime</scope>\n" +
-                    "        </dependency>\n" +
-                    "    </dependencies>\n" +
-                    "    <repositories>\n" +
-                    "        <repository>\n" +
-                    "            <id>jcenter</id>\n" +
-                    "            <name>jcenter</name>\n" +
-                    "            <url>https://jcenter.bintray.com</url>\n" +
-                    "        </repository>\n" +
-                    "        <repository>\n" +
-                    "            <id>mavencentral</id>\n" +
-                    "            <name>mavencentral</name>\n" +
-                    "            <url>https://repo.maven.apache.org/maven2</url>\n" +
-                    "        </repository>\n" +
-                    "    </repositories>\n" +
-                    "</project>\n";
+            """
+                    <?xml version="1.0" encoding="UTF-8"?>
+                    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                             xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+                        <modelVersion>4.0.0</modelVersion>
+                        <groupId>org.springframework.sbm.examples</groupId>
+                        <artifactId>migrate-jax-rs</artifactId>
+                        <packaging>jar</packaging>
+                        <version>0.0.1-SNAPSHOT</version>
+                        <properties>
+                            <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                        </properties>
+                        <build>
+                            <plugins>
+                                <plugin>
+                                    <groupId>org.apache.maven.plugins</groupId>
+                                    <artifactId>maven-compiler-plugin</artifactId>
+                                    <version>3.5.1</version>
+                                    <configuration>
+                                        <source>1.8</source>
+                                        <target>1.8</target>
+                                    </configuration>
+                                </plugin>
+                            </plugins>
+                        </build>
+                        <dependencies>
+                            <dependency>
+                                <groupId>org.springframework.boot</groupId>
+                                <artifactId>spring-boot-starter-web</artifactId>
+                                <version>2.3.4.RELEASE</version>
+                            </dependency>
+                            <dependency>
+                                <groupId>org.jboss.spec.javax.ws.rs</groupId>
+                                <artifactId>jboss-jaxrs-api_2.1_spec</artifactId>
+                                <version>1.0.1.Final</version>
+                                <scope>runtime</scope>
+                            </dependency>
+                        </dependencies>
+                        <repositories>
+                            <repository>
+                                <id>jcenter</id>
+                                <name>jcenter</name>
+                                <url>https://jcenter.bintray.com</url>
+                            </repository>
+                            <repository>
+                                <id>mavencentral</id>
+                                <name>mavencentral</name>
+                                <url>https://repo.maven.apache.org/maven2</url>
+                            </repository>
+                        </repositories>
+                    </project>
+                    """;
 
 
     @Test
