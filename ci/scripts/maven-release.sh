e46f8284c72a2e815be818849f73d365498df6ba
Fabian Krüger <fkrueger@vmware.com>
2023-03-04 01:13:43 +0700

    ci: Moving dirs to execute from project root

--- ci/scripts/maven-release.sh
+++ ci/scripts/maven-release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
+pushd git-repo > /dev/null
 mvn --batch-mode -Dtag=0.14.0 release:prepare \
                  -DreleaseVersion=v0.14.0 \
                  -DdevelopmentVersion=0.14.1-SNAPSHOT
 
 mvn release:clean
+popd > /dev/null
