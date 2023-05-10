a1ea80be1957e4fbe46d4217d428b451b56f73da
sanagaraj-pivotal <sanagaraj@pivotal.io>
2023-05-10 11:46:39 +0100

    Arch rule: Exclude test classes

--- components/sbm-core/src/test/java/org/springframework/sbm/architecture/ControlledInstantiationOfExecutionContextTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/architecture/ControlledInstantiationOfExecutionContextTest.java
@@ -3,17 +3,20 @@ package org.springframework.sbm.architecture;
 import com.tngtech.archunit.core.domain.AccessTarget;
 import com.tngtech.archunit.core.domain.JavaCall;
 import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.runner.RunWith;
 import org.openrewrite.ExecutionContext;
+import org.springframework.sbm.openrewrite.RewriteExecutionContext;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 @RunWith(ArchUnitRunner.class)
-@AnalyzeClasses(packages = "org.springframework.sbm")
+@AnalyzeClasses(packages = "org.springframework.sbm", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ControlledInstantiationOfExecutionContextTest {
 
     @ArchTest
@@ -27,5 +30,6 @@ public class ControlledInstantiationOfExecutionContextTest {
                                                     JavaClass.Predicates.assignableTo(ExecutionContext.class)
                                             ))
                             )
-                    );
+                    )
+            ;
 }
