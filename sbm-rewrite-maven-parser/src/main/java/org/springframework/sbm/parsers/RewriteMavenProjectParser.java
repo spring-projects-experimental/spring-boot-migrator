c313034a7da329b274b6a6dba358867f12480d5d
Fabian Krüger <fkrueger@vmware.com>
2023-07-06 12:07:19 +0200

    Add copyright headers

--- sbm-rewrite-maven-parser/src/main/java/org/springframework/sbm/parsers/RewriteMavenProjectParser.java
+++ sbm-rewrite-maven-parser/src/main/java/org/springframework/sbm/parsers/RewriteMavenProjectParser.java
@@ -15,10 +15,12 @@
  */
 package org.springframework.sbm.parsers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.maven.Maven;
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.DefaultArtifactRepository;
 import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
 import org.apache.maven.execution.*;
 import org.apache.maven.graph.DefaultProjectDependencyGraph;
@@ -27,6 +29,8 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.apache.maven.project.*;
+import org.apache.maven.repository.LocalArtifactRepository;
+import org.apache.maven.repository.UserLocalArtifactRepository;
 import org.apache.maven.rtinfo.RuntimeInformation;
 import org.apache.maven.settings.crypto.SettingsDecrypter;
 import org.codehaus.plexus.*;
@@ -59,10 +63,12 @@ import static java.util.stream.Collectors.toList;
 /**
  * @author Fabian Krüger
  */
+@Slf4j
 @Component
 public class RewriteMavenProjectParser {
 
     public static final String LOCAL_REPOSITORY = Path.of(System.getProperty("user.home")).resolve(".m2").resolve("repository").toString();
+    public static final List<String> MAVEN_GOALS = List.of("clean", "test-compile");// "dependency:resolve";
 
 
     public RewriteProjectParsingResult parse(Path baseDir) {
@@ -168,9 +174,9 @@ public class RewriteMavenProjectParser {
     private void runInMaven(Path baseDir, PlexusContainer plexusContainer, Consumer<MavenSession> sessionConsumer) {
         try {
             MavenExecutionRequest request = new DefaultMavenExecutionRequest();
-            ArtifactRepositoryFactory repositoryFactory = null;
-            repositoryFactory = plexusContainer.lookup(ArtifactRepositoryFactory.class);
-            ArtifactRepository repository = repositoryFactory.createArtifactRepository("local", "file://" + LOCAL_REPOSITORY, new DefaultRepositoryLayout(), null, null); // new MavenArtifactRepository("local", "file://"+LOCAL_REPOSITORY, new DefaultRepositoryLayout(), null, null);
+            ArtifactRepositoryFactory repositoryFactory = plexusContainer.lookup(ArtifactRepositoryFactory.class);
+            ArtifactRepository repository = new UserLocalArtifactRepository(repositoryFactory.createArtifactRepository("local", "file://" + LOCAL_REPOSITORY, new DefaultRepositoryLayout(), null, null));// repositoryFactory.createArtifactRepository("local", "file://" + LOCAL_REPOSITORY, new DefaultRepositoryLayout(), null, null); // new MavenArtifactRepository("local", "file://"+LOCAL_REPOSITORY, new DefaultRepositoryLayout(), null, null);
+            repository.setUrl("file://" + LOCAL_REPOSITORY);
             request.setBaseDirectory(baseDir.toFile());
             request.setLocalRepositoryPath(LOCAL_REPOSITORY);
             request.setActiveProfiles(List.of("default")); // TODO: make profile configurable
@@ -186,7 +192,7 @@ public class RewriteMavenProjectParser {
             request.setLoggingLevel(MavenExecutionRequest.LOGGING_LEVEL_DEBUG);
             request.setMultiModuleProjectDirectory(baseDir.toFile());
             request.setLocalRepository(repository);
-            request.setGoals(List.of("dependency:resolve"));
+            request.setGoals(MAVEN_GOALS);
             request.setPom(baseDir.resolve("pom.xml").toFile());
             request.setExecutionListener(new AbstractExecutionListener() {
                 @Override
@@ -199,10 +205,18 @@ public class RewriteMavenProjectParser {
                 @Override
                 public void sessionStarted(ExecutionEvent event) {
                     super.sessionStarted(event);
-                    sessionConsumer.accept(event.getSession());
                 }
 
-//                @Override
+                @Override
+                public void mojoSucceeded(ExecutionEvent event) {
+                    log.debug("Mojo succeeded: " + event.getMojoExecution().getGoal() + " in " + event.getMojoExecution().getLifecyclePhase());
+                    if(event.getMojoExecution().getGoal().equals("testCompile")) {
+                        log.info("Starting Maven session consumer");
+                        sessionConsumer.accept(event.getSession());
+                    }
+                }
+
+                //                @Override
 //                public void mojoSucceeded(ExecutionEvent event) {
 //
 //                    super.mojoSucceeded(event);
