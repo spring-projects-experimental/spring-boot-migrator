babb5acca444f31cab8ea5be59fbb2c7b3a35970
Fabian Krüger <fkrueger@vmware.com>
2023-07-12 00:31:16 +0200

    Moved MavenConfigFileParser to main

--- sbm-rewrite-maven-parser/src/main/java/org/springframework/sbm/parsers/MavenConfigFileParser.java
+++ sbm-rewrite-maven-parser/src/main/java/org/springframework/sbm/parsers/MavenConfigFileParser.java
@@ -39,7 +39,7 @@ import java.util.stream.Stream;
  * @author Fabian Krüger
  */
 @Component
-class MavenConfigFileParser {
+public class MavenConfigFileParser {
 
     public static final char ALTERNATE_POM_FILE = 'f';
 
