1967edb2308adb2ef0674d168f6ac2dbb0efd477
Fabian Krüger <fkrueger@vmware.com>
2023-08-03 21:32:57 +0200

    Add testcode

---
+++ sbm-support-rewrite/testcode/maven-projects/simple-spring-boot/src/main/java/com/example/simplespringboot/SimpleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootApplication.class, args);
+    }
+
+}
