9848d7e93249cfdf9061daf8173188e4641a49d4
Fabian Krüger <fkrueger@vmware.com>
2023-08-04 20:26:15 +0200

    Copied modules and fixed some compile errors

---
+++ rework/sbm-support-boot/testcode/empty-project/expected/src/main/java/org/springframework/sbm/SpringBootApp.java
@@ -0,0 +1,12 @@
+package org.springframework.sbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
