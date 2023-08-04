05d0edf94c2d7ff5584878f7faf2af74d4aca818
Fabian Krüger <fkrueger@vmware.com>
2023-08-04 21:18:31 +0200

    Add module sbm-support-jee and cleanup others

--- rework/sbm-support-jee/src/main/java/org/springframework/sbm/jee/web/api/JeeWebXmlProjectResourceRegistrar.java
+++ rework/sbm-support-jee/src/main/java/org/springframework/sbm/jee/web/api/JeeWebXmlProjectResourceRegistrar.java
@@ -15,9 +15,6 @@
  */
 package org.springframework.sbm.jee.web.api;
 
-import lombok.RequiredArgsConstructor;
-import org.openrewrite.ExecutionContext;
-import org.openrewrite.internal.InMemoryLargeSourceSet;
 import org.springframework.sbm.project.resource.ProjectResourceWrapper;
 import org.springframework.sbm.project.resource.RewriteSourceFileHolder;
 import org.springframework.sbm.project.web.api.WebAppType;
@@ -25,7 +22,6 @@ import org.openrewrite.SourceFile;
 import org.openrewrite.xml.search.FindTags;
 import org.openrewrite.xml.tree.Xml;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -34,10 +30,8 @@ import javax.xml.bind.Unmarshaller;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
+@Configuration
 public class JeeWebXmlProjectResourceRegistrar implements ProjectResourceWrapper<RewriteSourceFileHolder<Xml.Document>> {
-    private final ExecutionContext executionContext;
 
 //    @EventListener(ProjectContextBuiltEvent.class)
 //    public void onProjectContextBuiltEvent(ProjectContextBuiltEvent projectContextBuiltEvent) {
@@ -74,7 +68,7 @@ public class JeeWebXmlProjectResourceRegistrar implements ProjectResourceWrapper
         return (
                 Xml.Document.class.isAssignableFrom(rewriteSourceFileHolder.getSourceFile().getClass()) &&
                 rewriteSourceFileHolder.getAbsolutePath().getFileName().endsWith("web.xml") &&
-                ! new FindTags("/web-app").run(new InMemoryLargeSourceSet(List.of(rewriteSourceFileHolder.getSourceFile())), executionContext).getChangeset().getAllResults().isEmpty());
+                ! new FindTags("/web-app").run(List.of(rewriteSourceFileHolder.getSourceFile())).getResults().isEmpty());
     }
 
     @Override
