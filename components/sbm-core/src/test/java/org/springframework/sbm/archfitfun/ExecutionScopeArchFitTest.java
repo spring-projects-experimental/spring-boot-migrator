952136570536f3c753cf7fa403143d69c666d4b8
Fabian Krüger <fkrueger@vmware.com>
2023-08-05 00:29:18 +0200

    Fix compile errors

--- components/sbm-core/src/test/java/org/springframework/sbm/archfitfun/ExecutionScopeArchFitTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/archfitfun/ExecutionScopeArchFitTest.java
@@ -60,6 +60,7 @@ import org.springframework.sbm.scopes.ScanScope;
 import org.springframework.sbm.xml.parser.RewriteXmlParser;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.validation.beanvalidation.CustomValidatorBean;
+import org.springframework.sbm.parsers.RewriteMavenArtifactDownloader;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
