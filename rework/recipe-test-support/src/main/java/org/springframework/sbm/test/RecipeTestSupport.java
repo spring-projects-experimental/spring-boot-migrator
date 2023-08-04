68b26af8f15c0180b79f862bbe6f352444e0f592
Fabian Krüger <fkrueger@vmware.com>
2023-08-04 20:26:15 +0200

    Copied modules and fixed some compile errors

--- rework/recipe-test-support/src/main/java/org/springframework/sbm/test/RecipeTestSupport.java
+++ rework/recipe-test-support/src/main/java/org/springframework/sbm/test/RecipeTestSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 - 2023 the original author or authors.
+ * Copyright 2021 - 2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import jakarta.validation.Validator;
+import javax.validation.Validator;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Consumer;
---
+++ rework/sbm-support-boot/testcode/empty-project/expected/src/test/java/org/springframework/sbm/SpringBootAppTest.java
@@ -0,0 +1,13 @@
+package org.springframework.sbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
