0c3a310ce24568499bebc979d26e72070d204df5
Fabian Krüger <fkrueger@vmware.com>
2023-08-05 11:55:14 +0200

    Removed rework dir

--- rework/sbm-recipes-boot-upgrade/testcode/spring-boot-2.4-to-2.5-example/given/src/main/java/com/example/springboot24to25example/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot24to25example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-    public Tag getOne(Long id);
-}
