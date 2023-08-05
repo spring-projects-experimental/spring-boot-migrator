58fdbe0301d4db6636410ddbc0c91847e3ae6cd2
Fabian Krüger <fkrueger@vmware.com>
2023-08-05 20:18:14 +0200

    TestProjectContext persists resources

--- components/sbm-core/src/test/java/org/springframework/sbm/project/resource/TestProjectContext.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/project/resource/TestProjectContext.java
@@ -36,11 +36,13 @@ import org.springframework.sbm.project.parser.PathScanner;
 import org.springframework.sbm.project.parser.ProjectContextInitializer;
 import org.springframework.sbm.test.SpringBeanProvider;
 import org.springframework.sbm.test.TestProjectContextInfo;
+import org.springframework.sbm.utils.ResourceUtil;
 import org.springframework.validation.beanvalidation.CustomValidatorBean;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
@@ -468,11 +470,15 @@ public class TestProjectContext {
         /**
          * Serializes the built {@code ProjectContext} to {@code targetDir} and returns it.
          */
-        public ProjectContext serializeProjectContext(Path targetDir) {
+        public ProjectContext buildAndSerializeProjectContext(Path targetDir) {
             withProjectRoot(targetDir);
 
             ProjectContext projectContext = build();
 
+            return serializeProjectContext(projectContext);
+        }
+
+        private ProjectContext serializeProjectContext(ProjectContext projectContext) {
             ProjectContextSerializer serializer = new ProjectContextSerializer(
                     new ProjectResourceSetSerializer(new ProjectResourceSerializer()));
             projectContext.getProjectResources().stream().forEach(r -> r.markChanged());
@@ -507,6 +513,11 @@ public class TestProjectContext {
                 resourcesWithRelativePaths.put(Path.of("pom.xml"), xml);
             }
 
+            try {
+                projectRoot = Files.createTempDirectory(Path.of(System.getProperty("java.io.tmpdir")), "");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
             // create resource map with fully qualified paths
             Map<Path, String> resourcesWithAbsolutePaths = new LinkedHashMap<>();
@@ -544,7 +555,10 @@ public class TestProjectContext {
                                                                                     javaParser,
                                                                                     executionContext);
              */
+            writeResources(projectRoot, scannedResources);
+
             ProjectContextInitializer projectContextInitializer = createProjectContextInitializer();
+
 
             // create ProjectContext
             ProjectContext projectContext = projectContextInitializer.initProjectContext(projectRoot, scannedResources);
@@ -558,6 +572,21 @@ public class TestProjectContext {
             }
 
             return projectContext;
+        }
+
+        private void writeResources(Path projectRoot, List<Resource> scannedResources) {
+            scannedResources.stream()
+                    .forEach(r -> {
+                        try {
+                            Path path = ResourceUtil.getPath(r);
+                            if(!path.getParent().toFile().exists()) {
+                                Files.createDirectories(path.getParent());
+                            }
+                            Files.writeString(path, ResourceUtil.getContent(r));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
         }
 
         private void orderByOrderAnnotationValue(List<ProjectResourceWrapper> resourceWrapperList) {
