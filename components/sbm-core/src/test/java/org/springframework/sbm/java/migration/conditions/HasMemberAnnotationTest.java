ba687257104153817458db2c492cf52f21f7ec4e
Fabian Krüger <fkrueger@vmware.com>
2023-08-08 01:50:42 +0200

    Fix testdata in test which was surfaced by the new parser

--- components/sbm-core/src/test/java/org/springframework/sbm/java/migration/conditions/HasMemberAnnotationTest.java
+++ components/sbm-core/src/test/java/org/springframework/sbm/java/migration/conditions/HasMemberAnnotationTest.java
@@ -49,22 +49,21 @@ class HasMemberAnnotationTest {
     @Test
     void testIsApplicableFALSE() {
         String sourceCode =
-                "" +
-                        "import org.junit.jupiter.api.BeforeEach; " +
-                        "                                   " +
-                        "class AnnotatedClass {             " +
-                        "   @BeforeEach                     " +
-                        "   private int var1;               " +
-                        "}                                  " +
-                        "";
+                """
+                import javax.validation.constraints.Min;
+                class AnnotatedClass {
+                   @Min(1)
+                   private int var1;
+                }
+                """;
 
         ProjectContext context = TestProjectContext.buildProjectContext()
                 .withJavaSources(sourceCode)
-                .withBuildFileHavingDependencies("org.junit.jupiter:junit-jupiter-api:5.7.0")
+                .withBuildFileHavingDependencies("javax.validation:validation-api:2.0.1.Final")
                 .build();
 
         HasMemberAnnotation sut = new HasMemberAnnotation();
-        sut.setAnnotation("org.junit.jupiter.api.Test");
+        sut.setAnnotation("javax.validation.constraints.Max");
 
         assertThat(sut.evaluate(context)).isFalse();
     }
