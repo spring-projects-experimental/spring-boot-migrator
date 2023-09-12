4157012877d3b109f9cba5597215fdd77b4af8bb
Fabian Krüger <fkrueger@vmware.com>
2023-09-12 09:58:41 +0200

    Fix parsing duplicate pom files
    Added target to default ignorePatterns
    Refactor method names for test
    Fix dependency download
    Fix test by ignoring poms when parsing resources
    Add logging
    Replace List with Set to avoid duplicates

--- sbm-support-rewrite/src/test/java/org/springframework/sbm/parsers/RewriteProjectParserIntegrationTest.java
+++ sbm-support-rewrite/src/test/java/org/springframework/sbm/parsers/RewriteProjectParserIntegrationTest.java
@@ -50,6 +50,18 @@ public class RewriteProjectParserIntegrationTest {
         assertThat(parsingResult.sourceFiles().stream().map(sf -> sf.getSourcePath().toString()).toList()).contains("checkstyle/suppressions.xml");
     }
 
+    @Test
+    @DisplayName("parse4Modules")
+    void parse4Modules() {
+        Path baseDir = getMavenProject("4-modules");
+        List<Resource> resources = projectScanner.scan(baseDir);
+
+        assertThat(resources).hasSize(4);
+
+        RewriteProjectParsingResult parsingResult = sut.parse(baseDir, resources, new InMemoryExecutionContext(t -> {throw new RuntimeException(t);}));
+        assertThat(parsingResult.sourceFiles()).hasSize(4);
+    }
+
     private Path getMavenProject(String s) {
         return Path.of("./testcode/maven-projects/").resolve(s).toAbsolutePath().normalize();
     }
