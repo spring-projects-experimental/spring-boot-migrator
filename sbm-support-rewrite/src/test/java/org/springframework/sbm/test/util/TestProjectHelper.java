a23f58e4253a1305112c7f83054d399c804c6581
Fabian Krüger <fkrueger@vmware.com>
2023-11-01 14:43:36 +0100

    Merge TestHelpers

--- sbm-support-rewrite/src/test/java/org/springframework/sbm/test/util/TestProjectHelper.java
+++ sbm-support-rewrite/src/test/java/org/springframework/sbm/test/util/TestProjectHelper.java
@@ -15,13 +15,103 @@
  */
 package org.springframework.sbm.test.util;
 
+import org.apache.commons.io.FileUtils;
+import org.openrewrite.shaded.jgit.api.Git;
+import org.openrewrite.shaded.jgit.api.errors.GitAPIException;
+import org.springframework.core.io.Resource;
+import org.springframework.sbm.utils.ResourceUtil;
+
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Fabian Krüger
  */
 public class TestProjectHelper {
+    private final Path targetDir;
+    private List<Resource> resources = new ArrayList<>();
+    private boolean initializeGitRepo;
+    private String gitUrl;
+    private String gitTag;
+    private boolean deleteDirIfExists = false;
+
+    public TestProjectHelper(Path targetDir) {
+        this.targetDir = targetDir;
+    }
+
     public static Path getMavenProject(String s) {
         return Path.of("./testcode/maven-projects/").resolve(s).toAbsolutePath().normalize();
+    }
+
+    public static TestProjectHelper createTestProject(Path targetDir) {
+        return new TestProjectHelper(targetDir);
+    }
+
+    public static TestProjectHelper createTestProject(String targetDir) {
+        return new TestProjectHelper(Path.of(targetDir).toAbsolutePath().normalize());
+    }
+
+    public TestProjectHelper withResources(Resource... resources) {
+        this.resources.addAll(Arrays.asList(resources));
+        return this;
+    }
+
+    public TestProjectHelper initializeGitRepo() {
+        this.initializeGitRepo = true;
+        return this;
+    }
+
+    public TestProjectHelper cloneGitProject(String url) {
+        this.gitUrl = url;
+        return this;
+    }
+
+    public TestProjectHelper checkoutTag(String tag) {
+        this.gitTag = tag;
+        return this;
+    }
+
+    public TestProjectHelper deleteDirIfExists() {
+        this.deleteDirIfExists = true;
+        return this;
+    }
+
+    public void writeToFilesystem() {
+        if(deleteDirIfExists) {
+            try {
+                FileUtils.deleteDirectory(targetDir.toFile());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (initializeGitRepo) {
+            try {
+                Git.init().setDirectory(targetDir.toFile()).call();
+            } catch (GitAPIException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (gitUrl != null) {
+            try {
+                File directory = targetDir.toFile();
+                Git git = Git.cloneRepository()
+                        .setDirectory(directory)
+                        .setURI(this.gitUrl)
+                        .call();
+
+                if (gitTag != null) {
+                    git.checkout()
+                            .setName("refs/tags/" + gitTag)
+                            .call();
+                }
+            } catch (GitAPIException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        ResourceUtil.write(targetDir, resources);
     }
 }
