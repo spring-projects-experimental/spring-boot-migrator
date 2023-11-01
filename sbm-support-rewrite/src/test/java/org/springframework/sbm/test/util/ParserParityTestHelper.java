0abcb08baa9ecaec07de2cbc8acfc0f19f5493a7
Fabian Krüger <fkrueger@vmware.com>
2023-11-01 17:11:30 +0100

    Extracted helper to parse in parallel

--- sbm-support-rewrite/src/test/java/org/springframework/sbm/test/util/ParserParityTestHelper.java
+++ sbm-support-rewrite/src/test/java/org/springframework/sbm/test/util/ParserParityTestHelper.java
@@ -23,20 +23,11 @@ import org.openrewrite.marker.Marker;
 import org.openrewrite.marker.Markers;
 import org.openrewrite.maven.MavenSettings;
 import org.openrewrite.style.Style;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.sbm.boot.autoconfigure.SbmSupportRewriteConfiguration;
 import org.springframework.sbm.parsers.ParserProperties;
-import org.springframework.sbm.parsers.RewriteProjectParser;
 import org.springframework.sbm.parsers.RewriteProjectParsingResult;
-import org.springframework.sbm.parsers.maven.ComparingParserFactory;
-import org.springframework.sbm.parsers.maven.RewriteMavenProjectParser;
 
 import java.nio.file.Path;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
@@ -81,66 +72,39 @@ public class ParserParityTestHelper {
     }
 
     public void verifyParity(ParserResultParityChecker parserResultParityChecker) {
-        try {
-            RewriteProjectParsingResult comparingParserResult = null;
-            RewriteProjectParsingResult testedParserResult = null;
+        RewriteProjectParsingResult comparingParserResult = null;
+        RewriteProjectParsingResult testedParserResult = null;
 
-            if(isParallelParse) {
-                CountDownLatch latch = new CountDownLatch(2);
-
-                ExecutorService threadPool
-                        = Executors.newFixedThreadPool(2);
-
-                AtomicReference<RewriteProjectParsingResult> testedParsingResultRef = new AtomicReference<>();
-                AtomicReference<RewriteProjectParsingResult> comparingParsingResultRef = new AtomicReference<>();
-
-                threadPool.submit(() -> {
-                    System.out.println("Start parsing with RewriteProjectParser");
-                    RewriteProjectParsingResult tmpTestedParserResult = parseWithRewriteProjectParser(parserProperties, executionContext);
-                    testedParsingResultRef.set(tmpTestedParserResult);
-                    latch.countDown();
-                });
-
-                threadPool.submit(() -> {
-                    System.out.println("Start parsing with RewriteMavenProjectParser");
-                    RewriteProjectParsingResult tmpComparingParserResult = parseWithComparingParser(parserProperties, executionContext);
-                    comparingParsingResultRef.set(tmpComparingParserResult);
-                    latch.countDown();
-                });
-
-                latch.await();
-
-                comparingParserResult = comparingParsingResultRef.get();
-                testedParserResult = testedParsingResultRef.get();
-            } else {
-                testedParserResult = parseWithRewriteProjectParser(parserProperties, executionContext);
-                comparingParserResult = parseWithComparingParser(parserProperties, executionContext);
-            }
-
-
-
-            // Number of parsed sources should always be the same
-            assertThat(comparingParserResult.sourceFiles().size())
-                    .as(renderErrorMessage(comparingParserResult, testedParserResult))
-                    .isEqualTo(testedParserResult.sourceFiles().size());
-
-            // The paths of sources should be the same
-            List<String> comparingResultPaths = comparingParserResult.sourceFiles().stream().map(sf -> baseDir.resolve(sf.getSourcePath()).toAbsolutePath().normalize().toString()).toList();
-            List<String> testedResultPaths = testedParserResult.sourceFiles().stream().map(sf -> baseDir.resolve(sf.getSourcePath()).toAbsolutePath().normalize().toString()).toList();
-            assertThat(testedResultPaths).containsExactlyInAnyOrder(comparingResultPaths.toArray(String[]::new));
-
-            // The Markers of all resources should be the same
-            verifyMarkersAreTheSame(comparingParserResult, testedParserResult);
-
-            parserResultParityChecker.accept(testedParserResult, comparingParserResult);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        ParserExecutionHelper parserExecutionHelper = new ParserExecutionHelper();
+        if (isParallelParse) {
+            ParallelParsingResult result = parserExecutionHelper.parseParallel(baseDir, parserProperties, executionContext);
+            comparingParserResult = result.comparingParsingResult();
+            testedParserResult = result.testedParsingResult();
+        } else {
+            testedParserResult = parserExecutionHelper.parseWithRewriteProjectParser(baseDir, parserProperties, executionContext);
+            comparingParserResult = parserExecutionHelper.parseWithComparingParser(baseDir, parserProperties, executionContext);
         }
+
+
+        // Number of parsed sources should always be the same
+        assertThat(comparingParserResult.sourceFiles().size())
+                .as(renderErrorMessage(comparingParserResult, testedParserResult))
+                .isEqualTo(testedParserResult.sourceFiles().size());
+
+        // The paths of sources should be the same
+        List<String> comparingResultPaths = comparingParserResult.sourceFiles().stream().map(sf -> baseDir.resolve(sf.getSourcePath()).toAbsolutePath().normalize().toString()).toList();
+        List<String> testedResultPaths = testedParserResult.sourceFiles().stream().map(sf -> baseDir.resolve(sf.getSourcePath()).toAbsolutePath().normalize().toString()).toList();
+        assertThat(testedResultPaths).containsExactlyInAnyOrder(comparingResultPaths.toArray(String[]::new));
+
+        // The Markers of all resources should be the same
+        verifyMarkersAreTheSame(comparingParserResult, testedParserResult);
+
+        parserResultParityChecker.accept(testedParserResult, comparingParserResult);
     }
 
     private static String renderErrorMessage(RewriteProjectParsingResult comparingParserResult, RewriteProjectParsingResult testedParserResult) {
         List<SourceFile> collect = new ArrayList<>();
-        if(comparingParserResult.sourceFiles().size() > testedParserResult.sourceFiles().size()) {
+        if (comparingParserResult.sourceFiles().size() > testedParserResult.sourceFiles().size()) {
             collect = comparingParserResult.sourceFiles().stream()
                     .filter(element -> !testedParserResult.sourceFiles().contains(element))
                     .collect(Collectors.toList());
@@ -161,7 +125,7 @@ public class ParserParityTestHelper {
         comparingSourceFiles.sort(Comparator.comparing(SourceFile::getSourcePath));
         testedSourceFiles.sort(Comparator.comparing(SourceFile::getSourcePath));
 
-        for(SourceFile curComparingSourceFile : comparingSourceFiles) {
+        for (SourceFile curComparingSourceFile : comparingSourceFiles) {
             int index = comparingSourceFiles.indexOf(curComparingSourceFile);
             SourceFile curTestedSourceFile = testedSourceFiles.get(index);
 
@@ -188,7 +152,7 @@ public class ParserParityTestHelper {
                             Style.class)
                     .isEqualTo(testedMarkersList);
 
-            if(curComparingSourceFile.getMarkers().findFirst(JavaSourceSet.class).isPresent()) {
+            if (curComparingSourceFile.getMarkers().findFirst(JavaSourceSet.class).isPresent()) {
                 // Tested parser must have JavaSourceSet marker when comparing parser has it
                 assertThat(testedMarkers.findFirst(JavaSourceSet.class)).isPresent();
 
@@ -204,7 +168,7 @@ public class ParserParityTestHelper {
                                     .filter(element -> !testedClasspath.contains(element))
                                     .collect(Collectors.toList());
 
-                            if(!additionalElementsInComparingClasspath.isEmpty()) {
+                            if (!additionalElementsInComparingClasspath.isEmpty()) {
                                 return "Classpath of comparing and tested parser differ: comparing classpath contains additional entries: %s".formatted(additionalElementsInComparingClasspath);
                             }
 
@@ -212,7 +176,7 @@ public class ParserParityTestHelper {
                                     .filter(element -> !comparingClasspath.contains(element))
                                     .collect(Collectors.toList());
 
-                            if(!additionalElementsInTestedClasspath.isEmpty()) {
+                            if (!additionalElementsInTestedClasspath.isEmpty()) {
                                 return "Classpath of comparing and tested parser differ: tested classpath contains additional entries: %s".formatted(additionalElementsInTestedClasspath);
                             }
 
@@ -222,34 +186,6 @@ public class ParserParityTestHelper {
             }
 
         }
-    }
-
-    private RewriteProjectParsingResult parseWithComparingParser(ParserProperties parserProperties, ExecutionContext executionContext) {
-        RewriteMavenProjectParser comparingParser = new ComparingParserFactory().createComparingParser(parserProperties);
-        if(executionContext != null) {
-            return comparingParser.parse(baseDir, executionContext);
-        } else {
-            return comparingParser.parse(baseDir);
-        }
-    }
-
-    private RewriteProjectParsingResult parseWithRewriteProjectParser(ParserProperties parserProperties, ExecutionContext executionContext) {
-        AtomicReference<RewriteProjectParsingResult> atomicRef = new AtomicReference<>();
-        new ApplicationContextRunner().withUserConfiguration(SbmSupportRewriteConfiguration.class)
-                .withBean("parser-org.springframework.sbm.parsers.ParserProperties", ParserProperties.class, () -> parserProperties)
-                .run(appCtx -> {
-                    RewriteProjectParser sut = appCtx.getBean(RewriteProjectParser.class);
-                    RewriteProjectParsingResult testedParserResult = sut.parse(baseDir);
-                    atomicRef.set(testedParserResult);
-//                    RewriteProjectParsingResult parsingResult2 = sut.parse(tempDir);
-//                    verifyParsingResult(parsingResult2, RewriteProjectParserParityTest.ParserType.SBM);
-//
-//                    InMemoryExecutionContext executionContext1 = createExecutionContext();
-//                    RewriteMavenProjectParser mavenProjectParser = new ComparingParserFactory().createComparingParser(comparingParserProperties);
-//                    RewriteProjectParsingResult parsingResult = mavenProjectParser.parse(tempDir, executionContext1);
-//                    verifyParsingResult(parsingResult, RewriteProjectParserParityTest.ParserType.COMPARING);
-                });
-        return atomicRef.get();
     }
 
     public ParserParityTestHelper withExecutionContextForComparingParser(ExecutionContext executionContext) {
