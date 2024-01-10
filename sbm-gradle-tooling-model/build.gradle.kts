2e4c876da115587ab55e684c698d42905412a67e
Fabian Krüger <fkrueger@vmware.com>
2024-01-10 23:22:07 +0100

    Deleted moved modules

--- sbm-gradle-tooling-model/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("org.openrewrite.build.root") version("latest.release")
-}
-
-
-allprojects {
-    group = "org.springframework.sbm.gradle.tooling"
-    description = "A model for extracting semantic information out of Gradle build files necessary for refactoring them."
-}
